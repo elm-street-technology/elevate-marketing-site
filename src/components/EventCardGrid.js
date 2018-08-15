4edf3c120dc8fde0c8883fc04bc90c4be3df1fd0
Chris Heninger <chris.heninger@knledg.com>
2018-08-15 13:23:08 -0700

    Fancy hexagon background on events page

--- src/components/EventCardGrid.js
+++ src/components/EventCardGrid.js
@@ -10,9 +10,13 @@ export default withStyles((theme) => ({
   root: {
     display: "grid",
     justifyContent: "center",
-    gridTemplateColumns: "repeat(1, 400px)",
+    gridTemplateColumns: "repeat(1, 1fr)",
     gridColumnGap: "44px",
     gridRowGap: "32px",
+
+    [theme.breakpoints[600]]: {
+      gridTemplateColumns: "repeat(1, 400px)",
+    },
 
     [theme.breakpoints[900]]: {
       gridTemplateColumns: "repeat(2, 400px)",
--- src/pages/events.js
+++ src/pages/events.js
@@ -5,24 +5,41 @@ import EventCardGrid from "../components/EventCardGrid";
 import EventCard from "../components/EventCard";
 import Container from "../components/Container";
 import SEO from "../components/SEO";
+import Hexagons from "../images/hexagons.svg";
 
 const Events = ({ classes, data }) => {
   const events = data.allContentfulEvent.edges;
   return (
-    <div className={classes.container}>
-      <div className={classes.root}>
-        <SEO />
-        <Container>
-          <div className={classes.top}>
-            <div className={classes.heading}>Upcoming Events</div>
-            <Link to="/bootcamp">Learn about our Social Media Bootcamp →</Link>
-          </div>
-          <EventCardGrid className={classes.grid}>
-            {events.map(({ node: event }) => (
-              <EventCard key={event.id} event={event} />
-            ))}
-          </EventCardGrid>
-        </Container>
+    <div className={classes.root}>
+      <SEO />
+      <Container>
+        <div className={classes.top}>
+          <div className={classes.heading}>Upcoming Events</div>
+          <Link to="/bootcamp">Learn about our Social Media Bootcamp →</Link>
+        </div>
+        <EventCardGrid className={classes.grid}>
+          {events.map(({ node: event }) => (
+            <EventCard key={event.id} event={event} />
+          ))}
+        </EventCardGrid>
+      </Container>
+      <div className={classes.backgroundSlice}>
+        <svg
+          className={classes.backgroundTopSlice}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+        >
+          <polygon fill="white" points="0,100 100,0 100,100" />
+        </svg>
+        <svg
+          className={classes.backgroundBottomSlice}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 100 100"
+          preserveAspectRatio="none"
+        >
+          <polygon fill="white" points="0,100 100,0 100,100" />
+        </svg>
       </div>
     </div>
   );
@@ -45,6 +62,7 @@ export const query = graphql`
 
 export default withStyles((theme) => ({
   root: {
+    position: "relative",
     paddingTop: "96px",
     paddingBottom: "192px",
   },
@@ -60,5 +78,32 @@ export default withStyles((theme) => ({
   },
   grid: {
     paddingTop: "96px",
+  },
+  backgroundSlice: {
+    position: "absolute",
+    top: "240px",
+    bottom: "0",
+    right: "0",
+    left: "0",
+    width: "100%",
+    height: "400px",
+    backgroundImage: `url('${Hexagons}')`,
+    opacity: "0.5",
+    zIndex: "-1",
+  },
+
+  backgroundTopSlice: {
+    position: "absolute",
+    top: "-1px",
+    width: "100%",
+    height: "10vw",
+    transform: "rotate(180deg)",
+  },
+
+  backgroundBottomSlice: {
+    position: "absolute",
+    bottom: "0",
+    width: "100%",
+    height: "10vw",
   },
 }))(Events);
