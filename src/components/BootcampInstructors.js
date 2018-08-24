2355526b18bb1e1be6e1e745a83c0e56e6a5714a
Chris Heninger <heninger@gmail.com>
2018-08-24 08:34:37 -0700

    Finishing touches on bootcamp template (#17)
    * Begin finishing touches on bootcamp page
    
    * Clean up instructor cards, add signup content and form
    
    * Add responsive to signup section, add spacing to instructor heading

--- src/components/BootcampInstructors.js
+++ src/components/BootcampInstructors.js
@@ -3,6 +3,7 @@ import classNames from "classnames";
 import withStyles from "elevate-ui/withStyles";
 import ReactCardFlipper from "react-card-flipper";
 import ArrowForward from "elevate-ui/Icon/ArrowForward";
+import Plus from "../images/plus.svg";
 
 const BootcampInstructors = ({ classes, className, instructors }) => {
   return (
@@ -10,43 +11,41 @@ const BootcampInstructors = ({ classes, className, instructors }) => {
       <div className={classes.heading}>Meet your instructors</div>
       <div className={classes.instructorsGrid}>
         {instructors.map((item) => (
-          <ReactCardFlipper
-            key={item.id}
-            width={400}
-            height={400}
-            innerCardClass={classes.instructor}
-            levitate
-          >
-            <div className={classes.cardFront}>
-              <img
-                className={classes.instructorPhoto}
-                src={item.profilePhoto.file.url}
-                alt={"nice"}
-              />
-              <div className={classes.instructorName}>
-                <h2>
+          <div key={item.id}>
+            <ReactCardFlipper
+              width={300}
+              height={328}
+              innerCardClass={classes.instructor}
+              levitate
+            >
+              <div className={classes.cardFront}>
+                <img
+                  className={classes.instructorPhoto}
+                  src={item.profilePhoto.file.url}
+                  alt={"nice"}
+                />
+                <div className={classes.instructorName}>
                   {item.firstName} {item.lastName}
-                </h2>
+                </div>
+                <div className={classes.instructorTitle}>{item.jobTitle}</div>
+                <button className={classes.learnMore}>
+                  <span>Learn more about {item.firstName}</span>
+                  <ArrowForward size={18} color="#000" />
+                </button>
               </div>
-              <div className={classes.instructorTitle}>
-                <h3>{item.jobTitle}</h3>
+              <div>
+                <div
+                  className={classes.instructorBio}
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      item.bio &&
+                      item.bio.childMarkdownRemark &&
+                      item.bio.childMarkdownRemark.html,
+                  }}
+                />
               </div>
-              <button className={classes.learnMore}>
-                Learn more about {item.firstName} <ArrowForward color="#000" />
-              </button>
-            </div>
-            <div>
-              <div
-                className={classes.instructorBio}
-                dangerouslySetInnerHTML={{
-                  __html:
-                    item.bio &&
-                    item.bio.childMarkdownRemark &&
-                    item.bio.childMarkdownRemark.html,
-                }}
-              />
-            </div>
-          </ReactCardFlipper>
+            </ReactCardFlipper>
+          </div>
         ))}
       </div>
     </div>
@@ -69,6 +68,7 @@ export default withStyles((theme) => ({
     textAlign: "center",
   },
   instructorsGrid: {
+    position: "relative",
     flex: "1",
     display: "grid",
     gridTemplateColumns: "repeat(1, 1fr)",
@@ -79,21 +79,49 @@ export default withStyles((theme) => ({
     [theme.breakpoints[600]]: {
       gridTemplateColumns: "repeat(2, 1fr)",
     },
+
+    "& > *": {
+      "&:after": {
+        content: '""',
+        position: "absolute",
+        width: 300,
+        height: 328,
+        backgroundImage: `url('${Plus}')`,
+        backgroundSize: "45px 45px",
+        bottom: "-50px",
+        zIndex: "-10",
+      },
+    },
+
+    "& > *:nth-child(odd)": {
+      "&:after": {
+        left: "-44px",
+      },
+    },
+
+    "& > *:nth-child(even)": {
+      "&:after": {
+        right: "-44px",
+      },
+    },
   },
   instructor: {
     display: "flex",
     flexDirection: "column",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
     background: "#fff",
     borderRadius: theme.globalBorderRadius,
     border: "1px solid rgba(94, 94, 94, 0.05)",
     boxShadow: "0 8px 12px rgba(0,0,0,0.1)",
-    padding: "40px",
+    padding: "24px",
   },
   cardFront: {
     width: "100%",
     height: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
   instructorPhoto: {
     display: "block",
@@ -103,25 +131,23 @@ export default withStyles((theme) => ({
     borderRadius: "50%",
   },
   instructorName: {
-    "& h2": {
-      marginBottom: "16px",
-      fontWeight: "700",
-      fontSize: "24px",
-      color: "#000",
-      textAlign: "center",
-    },
+    marginBottom: "16px",
+    fontWeight: "700",
+    fontSize: "24px",
+    color: "#000",
+    textAlign: "center",
   },
   instructorTitle: {
-    "& h3": {
-      fontSize: "18px",
-      color: theme.colors.gray500,
-      textAlign: "center",
-    },
+    fontSize: "18px",
+    color: theme.colors.gray500,
+    textAlign: "center",
   },
   instructorBio: {
     "& p": {
-      fontSize: "14px",
+      fontSize: "13px",
       lineHeight: "18px",
+      marginTop: "4px",
+      marginBottom: "8px",
       color: theme.colors.gray800,
     },
   },
@@ -131,15 +157,16 @@ export default withStyles((theme) => ({
     alignItems: "center",
     justifyContent: "center",
     width: "100%",
-    maxWidth: "310px",
-    margin: "50px auto 10px",
-    padding: "23px",
-    borderRadius: theme.globalBorderRadius,
-    backgroundColor: "rgba(255	244	240)",
+    fontSize: "14px",
+    lineHeight: "18px",
     fontWeight: "700",
-    fontSize: "18px",
     textAlign: "center",
     color: theme.colors.primary,
+    backgroundColor: "rgba(255, 244, 240)",
+    borderRadius: theme.globalBorderRadius,
+    padding: "12px 8px",
+    marginTop: "auto",
+
     "& svg": {
       marginLeft: "10px",
     },
