9c2a676c3ab4b55e612b85c2d89244b8f71bc218
Jason Walsh <jason@walsh-web.com>
2018-10-18 15:54:49 -0700

    Add Basic Styles To Product Page

--- src/templates/product.js
+++ src/templates/product.js
@@ -145,6 +145,76 @@ export default withStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     width: "100%",
+
+    "& h1": {
+      color: "#2E2E35",
+      fontWeight: "700",
+      letterSpacing: ".2px",
+      lineHeight: "1.2",
+      fontSize: "24px",
+      marginTop: "44px",
+      marginBottom: "12px",
+    },
+
+    "& h2": {
+      fontSize: "22px",
+      lineHeight: "1.4",
+      fontWeight: "700",
+      marginTop: "32px",
+      marginBottom: "12px",
+    },
+
+    "& h3": {
+      fontSize: "20px",
+      fontWeight: "700",
+      marginTop: "32px",
+      marginBottom: "12px",
+    },
+
+    "& h4": {
+      fontSize: "18px",
+      fontWeight: "600",
+      marginTop: "24px",
+      marginBottom: "12px",
+    },
+
+    "& em": {
+      fontStyle: "italic",
+    },
+
+    "& strong": {
+      fontWeight: "700",
+    },
+
+    "& ol": {
+      position: "relative",
+      counterReset: "item",
+    },
+
+    "& ol li": {
+      counterIncrement: "item",
+      paddingLeft: "24px",
+
+      "&:before": {
+        content: 'counter(item) "."',
+        position: "absolute",
+        left: "0",
+        fontWeight: "700",
+      },
+    },
+
+    "& ul": {
+      listStyleType: "disc",
+    },
+
+    "& ul li": {
+      marginLeft: "18px",
+    },
+
+    "& p": {
+      marginTop: "4px",
+      marginBottom: "8px",
+    },
   },
   hero: {
     paddingTop: "36px",
