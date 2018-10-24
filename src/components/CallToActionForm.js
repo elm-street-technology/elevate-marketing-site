2b49859061514c02e00de6423fc35fb26d47152c
Jason Walsh <jason@walsh-web.com>
2018-10-23 17:34:06 -0700

    Call To Action - Success/Error States (#57)

--- src/components/CallToActionForm.js
+++ src/components/CallToActionForm.js
@@ -1,26 +1,95 @@
 import React from "react";
-import classNames from "classnames";
 import withStyles from "elevate-ui/withStyles";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import classNames from "classnames";
+import Alert from "elevate-ui/Alert";
 
-const Form = ({ props, children, classes, className }) => {
-  return (
-    <form className={classNames(classes.root, className)}>
-      <input
-        className={classes.input}
-        placeholder="Enter your email"
-        type="text"
+class CTAForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formState: null,
+    };
+  }
+
+  render() {
+    const formState = this.state.formState;
+    const { classes, className } = this.props;
+    if (formState === "success") {
+      return (
+        <div style={{ maxWidth: "400px", margin: "24px auto" }}>
+          <Alert color="success">We'll be in touch shortly!</Alert>
+        </div>
+      );
+    } else if (formState === "error") {
+      return (
+        <div style={{ maxWidth: "400px", margin: "24px auto" }}>
+          <Alert color="error">An error occurred.</Alert>
+        </div>
+      );
+    }
+    return (
+      <Formik
+        initialValues={{
+          email: "",
+        }}
+        validationSchema={() =>
+          Yup.object().shape({
+            email: Yup.string()
+              .email("Invalid email address")
+              .required("Email is required"),
+          })
+        }
+        onSubmit={(values, { setSubmitting }) => {
+          return fetch(
+            "https://relegate.herokuapp.com/marketing/request-a-demo",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json; charset=utf-8",
+              },
+              body: JSON.stringify({
+                email: values.email,
+              }),
+            }
+          )
+            .then((response) => response.json())
+            .then((res) => {
+              if (res.message === "ok") {
+                this.setState({ formState: "success" });
+              } else {
+                this.setState({ formState: "error" });
+              }
+            })
+            .catch((err) => {
+              this.setState({ formState: "error" });
+            });
+        }}
+        render={({ isSubmitting }) => (
+          <Form noValidate className={classNames(classes.root, className)}>
+            <Field id="email" name="email" className={classes.input} />
+            <button
+              type="submit"
+              className={classes.button}
+              disabled={isSubmitting}
+            >
+              Request a demo
+            </button>
+          </Form>
+        )}
       />
-      <button className={classes.button}>Request a demo</button>
-    </form>
-  );
-};
+    );
+  }
+}
 
 export default withStyles((theme) => ({
   root: {
     position: "relative",
     display: "flex",
-    alignItems: "center",
+    alignItems: "flex-start",
     width: "100%",
+    margin: "0",
     height: "48px",
     borderRadius: "4px",
     overflow: "hidden",
@@ -28,6 +97,7 @@ export default withStyles((theme) => ({
   input: {
     width: "100%",
     height: "48px",
+    margin: "0",
     padding: "18px 0 18px 15px",
     border: "1px solid rgba(100, 97, 110, .36)",
     borderRight: "0",
@@ -52,4 +122,4 @@ export default withStyles((theme) => ({
     textAlign: "center",
     cursor: "pointer",
   },
-}))(Form);
+}))(CTAForm);
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -4,106 +4,138 @@ import * as Yup from "yup";
 import withStyles from "elevate-ui/withStyles";
 import Input from "elevate-ui/Input";
 import classNames from "classnames";
+import Alert from "elevate-ui/Alert";
 
-const SignUpForm = ({ classes, className }) => (
-  <div className={classNames(classes.root, className)}>
-    <Formik
-      initialValues={{
-        firstname: "",
-        lastname: "",
-        company: "",
-        email: "",
-        phone: "",
-      }}
-      validationSchema={() =>
-        Yup.object().shape({
-          firstname: Yup.string().required("First name is required"),
-          lastname: Yup.string().required("Last name is required"),
-          company: Yup.string(),
-          email: Yup.string()
-            .email("Invalid email address")
-            .required("Email is required"),
-          phone: Yup.string().required("Phone is required"),
-        })
-      }
-      onSubmit={(values, { setSubmitting }) => {
-        return fetch("https://relegate.herokuapp.com/marketing/sign-up", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json; charset=utf-8",
-          },
-          body: JSON.stringify({
-            firstname: values.firstname,
-            lastname: values.lastname,
-            company: values.company,
-            email: values.email,
-            phone: values.phone,
-          }),
-        })
-          .then((response) => response.json())
-          .then((res) => {
-            if (res.message === "ok") {
-              setSubmitting(false); // success
-            } else {
-              // bad things happened, we don't know what, show generic error flash
-            }
-          })
-          .catch((err) => {
-            // bad things happened, we don't know what, show generic error flash
-          });
-      }}
-      render={({ isSubmitting }) => (
-        <Form noValidate style={{ maxWidth: "420px" }}>
-          <div className={classes.topRow}>
-            <Field
-              id="firstname"
-              name="firstname"
-              label="First Name"
-              component={Input}
-              className={classes.field}
-            />
-            <Field
-              id="lastname"
-              name="lastname"
-              label="Last Name"
-              component={Input}
-              className={classes.field}
-            />
-          </div>
-          <Field
-            id="company"
-            name="company"
-            label="Company"
-            component={Input}
-            className={classes.field}
-          />
-          <Field
-            id="email"
-            name="email"
-            label="Email"
-            component={Input}
-            className={classes.field}
-          />
-          <Field
-            id="phone"
-            name="phone"
-            label="Phone"
-            component={Input}
-            className={classes.field}
-            type="tel"
-          />
-          <button
-            type="submit"
-            className={classes.signUpBtn}
-            disabled={isSubmitting}
-          >
-            Request a demo
-          </button>
-        </Form>
-      )}
-    />
-  </div>
-);
+class SignUpForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formState: null,
+    };
+  }
+
+  render() {
+    const { formState } = this.state;
+    const { classes, className } = this.props;
+
+    if (formState === "success") {
+      return (
+        <div style={{ maxWidth: "400px", margin: "24px auto" }}>
+          <Alert color="success">We'll be in touch shortly!</Alert>
+        </div>
+      );
+    } else if (formState === "error") {
+      return (
+        <div style={{ maxWidth: "400px", margin: "24px auto" }}>
+          <Alert color="error">An error occurred.</Alert>
+        </div>
+      );
+    }
+
+    return (
+      <div className={classNames(classes.root, className)}>
+        <Formik
+          initialValues={{
+            firstname: "",
+            lastname: "",
+            company: "",
+            email: "",
+            phone: "",
+          }}
+          validationSchema={() =>
+            Yup.object().shape({
+              firstname: Yup.string().required("First name is required"),
+              lastname: Yup.string().required("Last name is required"),
+              company: Yup.string(),
+              email: Yup.string()
+                .email("Invalid email address")
+                .required("Email is required"),
+              phone: Yup.string().required("Phone is required"),
+            })
+          }
+          onSubmit={(values, { setSubmitting }) => {
+            return fetch(
+              "https://relegate.herokuapp.com/marketing/request-a-demo",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8",
+                },
+                body: JSON.stringify({
+                  firstname: values.firstname,
+                  lastname: values.lastname,
+                  company: values.company,
+                  email: values.email,
+                  phone: values.phone,
+                }),
+              }
+            )
+              .then((response) => response.json())
+              .then((res) => {
+                if (res.message === "ok") {
+                  this.setState({ formState: "success" });
+                } else {
+                  this.setState({ formState: "error" });
+                }
+              })
+              .catch((err) => {
+                this.setState({ formState: "error" });
+              });
+          }}
+          render={({ isSubmitting }) => (
+            <Form noValidate style={{ maxWidth: "420px" }}>
+              <div className={classes.topRow}>
+                <Field
+                  id="firstname"
+                  name="firstname"
+                  label="First Name"
+                  component={Input}
+                  className={classes.field}
+                />
+                <Field
+                  id="lastname"
+                  name="lastname"
+                  label="Last Name"
+                  component={Input}
+                  className={classes.field}
+                />
+              </div>
+              <Field
+                id="company"
+                name="company"
+                label="Company"
+                component={Input}
+                className={classes.field}
+              />
+              <Field
+                id="email"
+                name="email"
+                label="Email"
+                component={Input}
+                className={classes.field}
+              />
+              <Field
+                id="phone"
+                name="phone"
+                label="Phone"
+                component={Input}
+                className={classes.field}
+                type="tel"
+              />
+              <button
+                type="submit"
+                className={classes.signUpBtn}
+                disabled={isSubmitting}
+              >
+                Request a demo
+              </button>
+            </Form>
+          )}
+        />
+      </div>
+    );
+  }
+}
 
 export default withStyles((theme) => ({
   root: {
