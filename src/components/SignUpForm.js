407c887d53767e15ea7b55bf25693eca74794ce1
Chris Heninger <chris.heninger@knledg.com>
2018-10-30 18:39:14 -0700

    Updates to Get Started form

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import classNames from "classnames";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
-import withStyles from "elevate-ui/withStyles";
-import Input from "elevate-ui/Input";
-import classNames from "classnames";
 import Alert from "elevate-ui/Alert";
+import CheckboxGroup from "elevate-ui/CheckboxGroup";
+import Input from "elevate-ui/Input";
+import RadioGroup from "elevate-ui/RadioGroup";
+import Typography from "elevate-ui/Typography";
+import withStyles from "elevate-ui/withStyles";
 
-class SignUpForm extends React.Component {
+class SignUpForm extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -20,8 +23,28 @@ class SignUpForm extends React.Component {
 
     if (formState === "success") {
       return (
-        <div style={{ maxWidth: "400px", margin: "24px auto" }}>
-          <Alert color="success">We'll be in touch shortly!</Alert>
+        <div
+          style={{
+            maxWidth: "400px",
+            textAlign: "center",
+            margin: "24px auto",
+          }}
+        >
+          <Alert color="success">
+            <Typography type="heading3" gutterBottom>
+              We’re here to help!
+            </Typography>
+            <Typography type="heading6" gutterBottom>
+              An Elevate Success Coach will reach out to you asap to start you
+              on the path to pure productivity.
+            </Typography>
+            <Typography type="heading4" gutterTop>
+              Can’t wait? Talk NOW at{" "}
+              <a href="tel:18445469275" className={classes.link}>
+                844.546.9275
+              </a>
+            </Typography>
+          </Alert>
         </div>
       );
     } else if (formState === "error") {
@@ -41,6 +64,9 @@ class SignUpForm extends React.Component {
             company: "",
             email: "",
             phone: "",
+            role: "",
+            roleOther: "",
+            interests: [],
           }}
           validationSchema={() =>
             Yup.object().shape({
@@ -51,9 +77,16 @@ class SignUpForm extends React.Component {
                 .email("Invalid email address")
                 .required("Email is required"),
               phone: Yup.string().required("Phone is required"),
+              role: Yup.string(),
+              roleOther: Yup.string(),
+              interests: Yup.array(),
             })
           }
           onSubmit={(values, { setSubmitting }) => {
+            const body = {
+              ...values,
+              roleOther: values.role === "Other" ? values.roleOther : "", // Just in case the user had typed in roleOther then changed their role to something else
+            };
             return fetch(
               "https://relegate.herokuapp.com/marketing/request-a-demo",
               {
@@ -61,13 +94,7 @@ class SignUpForm extends React.Component {
                 headers: {
                   "Content-Type": "application/json; charset=utf-8",
                 },
-                body: JSON.stringify({
-                  firstname: values.firstname,
-                  lastname: values.lastname,
-                  company: values.company,
-                  email: values.email,
-                  phone: values.phone,
-                }),
+                body: JSON.stringify(body),
               }
             )
               .then((response) => response.json())
@@ -82,8 +109,18 @@ class SignUpForm extends React.Component {
                 this.setState({ formState: "error" });
               });
           }}
-          render={({ isSubmitting }) => (
-            <Form noValidate style={{ maxWidth: "420px" }}>
+          render={({ values, isSubmitting }) => (
+            <Form noValidate>
+              <Typography
+                type="heading6"
+                style={{ textAlign: "center", marginBottom: "32px" }}
+              >
+                Talk NOW at{" "}
+                <a href="tel:18445469275" className={classes.link}>
+                  844.546.9275
+                </a>{" "}
+                or schedule a demo:
+              </Typography>
               <div className={classes.topRow}>
                 <Field
                   id="firstname"
@@ -101,13 +138,6 @@ class SignUpForm extends React.Component {
                 />
               </div>
               <Field
-                id="company"
-                name="company"
-                label="Company"
-                component={Input}
-                className={classes.field}
-              />
-              <Field
                 id="email"
                 name="email"
                 label="Email"
@@ -122,12 +152,80 @@ class SignUpForm extends React.Component {
                 className={classes.field}
                 type="tel"
               />
+              <Field
+                id="company"
+                name="company"
+                label="Affiliation"
+                component={Input}
+                className={classes.field}
+              />
+              <Field
+                id="role"
+                name="role"
+                label="I am a(n)"
+                component={RadioGroup}
+                display="inline"
+                items={[
+                  {
+                    label: "Agent",
+                    value: "Agent",
+                  },
+                  {
+                    label: "Team",
+                    value: "Team",
+                  },
+                  {
+                    label: "Broker",
+                    value: "Broker",
+                  },
+                  {
+                    label: "Other (please specify)*",
+                    value: "Other",
+                  },
+                ]}
+                className={classes.field}
+              />
+              {values.role === "Other" && (
+                <Field
+                  id="roleOther"
+                  name="roleOther"
+                  label="Other"
+                  component={Input}
+                  className={classes.field}
+                  autoFocus
+                />
+              )}
+              <Field
+                id="interests"
+                name="interests"
+                label="I am interested in:  (select all that apply)"
+                component={CheckboxGroup}
+                items={[
+                  {
+                    label: "Managing my leads from start to close",
+                    value: "Managing my leads from start to close",
+                  },
+                  {
+                    label: "Capturing more leads through online marketing",
+                    value: "Capturing more leads through online marketing",
+                  },
+                  {
+                    label: "Consolidating my technology into ONE location",
+                    value: "Consolidating my technology into ONE location",
+                  },
+                  {
+                    label: "Closing more deals through smart insights",
+                    value: "Closing more deals through smart insights",
+                  },
+                ]}
+                className={classes.field}
+              />
               <button
                 type="submit"
                 className={classes.signUpBtn}
                 disabled={isSubmitting}
               >
-                Request a demo
+                Please contact me
               </button>
             </Form>
           )}
@@ -142,9 +240,12 @@ export default withStyles((theme) => ({
     display: "flex",
     flexDirection: "column",
     width: "100%",
-    maxWidth: "400px",
+    maxWidth: "500px",
     backgroundColor: "#FFF !important",
     margin: "0 auto",
+  },
+  link: {
+    color: "inherit",
   },
   field: {
     borderRadius: "6px",
@@ -167,7 +268,7 @@ export default withStyles((theme) => ({
     lineHeight: "26px",
     fontWeight: "600",
     color: "#FFF",
-    backgroundColor: "#40D0A3",
+    backgroundColor: theme.colors.secondary,
     borderRadius: "6px",
     padding: "12px",
     margin: "14px 0",
