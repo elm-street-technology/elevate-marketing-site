82ea6e3f4fb160e458257ec6aa5de74a12902847
Dillon Morris <dmorris@elmstreettechnology.com>
2018-10-31 11:14:21 -0700

    Updated role card subheadings and adjusted styling slightly

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,13 +25,12 @@ class Index extends Component {
     const roles = {
       Agents: {
         name: "Agents",
-        descriptionHeading: "Increase your day-to-day productivity",
+        tagline: "Increase your day-to-day productivity",
         description:
           "Lead the pack with an unparalleled marketing strategy & toolset that helps you build your online brand & close more business…all while giving you extra time every day for the things that matter most.",
         icon: "Person",
         color: "#F15953",
         borderColor: "rgba(241, 89, 83, .4)",
-        tagline: "I'm an agent",
         grid: [
           "I need help with my marketing: website, social, email",
           "I want to close more leads faster",
@@ -40,13 +39,12 @@ class Index extends Component {
       },
       Teams: {
         name: "Teams",
-        descriptionHeading: "Excel at agent adoption & retention",
+        tagline: "Excel at agent adoption & retention",
         description:
           "Arm yourself and your agents with easy-to-access, revenue-driving data that helps support smarter business decisions.",
         icon: "People",
         color: "#55C3BA",
         borderColor: "rgba(85, 195, 186, .4)",
-        tagline: "I lead a team of agents",
         grid: [
           "I want at-a-glance insight into what's happening across my brokerage",
           "I need assistance recruiting & retaining clients",
@@ -55,13 +53,12 @@ class Index extends Component {
       },
       Brokerages: {
         name: "Brokers",
-        descriptionHeading: "Streamline all aspects of your business",
+        tagline: "Streamline all aspects of your business",
         description:
           "Take the guess work out of your daily business operations, agent productivity and brand presence with ONE solution that is easy-to-implement and includes fanatical ongoing training & support.",
         icon: "GroupAdd",
         color: "#FFC629",
         borderColor: "rgba(255, 198, 41, .4)",
-        tagline: "I'm a broker",
         grid: [
           "We want to streamline lead routing & internal communication",
           "We want to better understand what drives our revenue",
