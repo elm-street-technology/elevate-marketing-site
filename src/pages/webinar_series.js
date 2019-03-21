92ecb0b7528d914a09f5ee643e1e9e00735c5d3c
Matt Litchliter <mattlitchliter@Matts-MacBook-Pro.local>
2019-03-21 15:25:44 -0500

    Adding 3 new pages

---
+++ src/pages/webinar_series.js
@@ -0,0 +1,106 @@
+import React from "react";
+import Link from "gatsby-link";
+import withStyles from "elevate-ui/withStyles";
+import Container from "../components/Container";
+import SEO from "../components/SEO";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Section2Col from "../components/Section2Col";
+import Section1Col from "../components/Section1Col";
+import PageTitleImage from "../components/PageTitleImage";
+import BgSection from "../components/BgSection";
+import TestImage from "../images/600.jpg";
+import WideImage from "../images/1200.jpg";
+import exImage from "../images/example.png";
+import exImage2 from "../images/example2.png";
+import exImage3 from "../images/example3.png";
+import bgImage from "../images/examplebg.jpg";
+
+const Demo = ({ classes }) => {
+
+  return (
+    <div className={classes.root}>
+      <Header />
+      <SEO />
+
+      <PageTitleImage title="The power of LEARNING" image={WideImage}>
+      Explore our <span style={{ fontWeight: "bold" }}>online education series</span><br />
+      from the comfort of your couch!
+      </PageTitleImage>
+
+      <div style={{ margin: "45px", }}>
+        <Section2Col secTitle="Section Header" secImage={TestImage}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <Link to="/upgrade" className={classes.ctabutton}>
+          Upgrade Button Text2
+                    </Link>
+                    <Link to="/upgrade" className={classes.ctabutton}>
+          Upgrade Button Text3
+                    </Link>
+        </Section2Col>
+      </div>
+    
+      <div style={{ margin: "45px", }}>
+        <Section1Col secTitle="Center Header">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        
+          <Link to="/upgrade" className={classes.ctabutton}>
+          Upgrade Button Text3
+                    </Link>
+
+        </Section1Col>
+      </div>
+
+        <BgSection bgImage={bgImage} secTitle="Section Header" secLink="/upgrade" secButton="Upgrade Button Text" secImage={exImage}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </BgSection>
+
+
+        <BgSection bgImage={bgImage} secTitle="Section Header" secLink="/upgrade" secButton="Upgrade Button Text" secImage={exImage3}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </BgSection>
+
+      <Footer />
+    </div>
+  );
+};
+
+
+
+
+
+
+
+export default withStyles((theme) => ({
+  root: {
+    position: "relative",
+    paddingTop: "0px",
+    paddingBottom: "0px",
+
+    [theme.breakpoints[900]]: {
+        paddingTop: "48px",
+    },
+  },
+  ctabutton: {
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "16px",
+    textAlign: "center",
+    fontWeight: "500",
+    minWidth: "300px",
+    letterSpacing: ".25px",
+    backgroundColor: theme.colors.secondary,
+    color: "#FFF",
+    display: "block",
+    marginTop: "30px",
+    maxWidth: "300px",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "3px",
+},
+
+
+
+
+
+}))(Demo);
--- src/templates/bootcamp.js
+++ src/templates/bootcamp.js
@@ -7,6 +7,9 @@ import Container from "../components/Container";
 import BootcampFaqs from "../components/BootcampFaqs";
 import BootcampSignup from "../components/BootcampSignup";
 import BootcampTopics from "../components/BootcampTopics";
+import PageTitleImage from "../components/PageTitleImage";
+import WideImage from "../images/1200.jpg";
+import Link from "gatsby-link";
 import BootcampInstructors from "../components/BootcampInstructors";
 import SMBCLogo from "../images/smbcLogo.jpg";
 // import SEO from '../components/SEO'
@@ -35,34 +38,87 @@ const Bootcamp = ({ classes, data: { contentfulBootcamp } }) => {
       </Helmet>
       {/* <SEO pagePath={slug} postNode={postNode} pageSEO /> */}
 
-      <Container>
-        <div style={{textAlign:"center",paddingTop:"40px"}}>
-          <iframe width="800" height="450" src="https://www.youtube.com/embed/fGRZ8-mLwnQ?autoplay=1&rel=0"
+
+      <PageTitleImage title="The power of KNOWLEDGE" image={WideImage}>
+      The leading social media boot camp provides agents with tips, tricks<br />
+      &amp; strategies to conquer social marketing, all while having a great time.
+      </PageTitleImage>
+
+      <div style={{paddingTop:"30px", paddingBottom:"60px",}}>
+        <Container>
+            <div className={classes.flexContainer}>
+                <div className={classes.flexItem}>
+
+                   
+                <iframe width="100%" height="350" src="https://www.youtube.com/embed/fGRZ8-mLwnQ?autoplay=1&rel=0"
           frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    
+
+                </div>
+                <div className={classes.flexItem} style={{paddingLeft:"25px", paddingRight:"25px", paddingBottom:"25px"}}>
+                <div className={classes.leftheading}>The Leading<br />
+                <span style={{ fontWeight: "bold" }}>Social Media Boot Camp</span></div>
+                <div className={classes.leftsubtext}>in real estate energizes and empowers agents to take control of their social marketing with tips, tricks &amp; strategies from experts across the industry!
+                <br /><br />
+                <span style={{ fontWeight: "bold" }}>Are we coming to your town?</span>
+                <Link to="/upgrade" className={classes.ctabutton}>
+                  View Upcoming Schedule</Link>
+                </div>
+
+                </div>
+            </div>
+        </Container>
         </div>
-        <div className={classes.titleText}>
-          Free Social Media Seminar <span style={{ color:"#f3574e"}}>Exclusively</span> for Real Estate Professionals
+
+
+
+      <Container>
+        <div style={{ textAlign: "center",paddingBottom:"40px", }}>
+          <img src={SMBCLogo} width="400" style={{paddingBottom:"15px"}}/>
+          <div className={classes.centerheading}>Want to book a <span style={{ fontWeight: "bold" }}>Boot Camp for your agents?</span></div>
+          <div style={{paddingTop:"40px", paddingBottom:"40px"}}>form here</div>
         </div>
-        <div style={{ textAlign: "center",paddingBottom:"40px" }}>
-          <img src={SMBCLogo} width="500" />
+      </Container>
+
+      <div style={{backgroundColor:"#f7f7f7"}}><Container>
+                <div style={{textAlign:"center", paddingTop:"45px", paddingBottom:"10px",}}>
+
+                   
+                <div className={classes.centerheading}><span style={{ fontWeight: "bold" }}>Frequently Asked Questions</span></div>
+                <div className={classes.centersubtext}>about our Boot Camp Series</div>
+                    
+
+                </div>
+  </Container>
+  </div>
+  <div style={{paddingTop:"20px", paddingBottom:"50px", backgroundColor:"#f7f7f7"}}>
+        <Container>
+            <div className={classes.flexContainer}>
+                <div className={classes.flexItem} style={{padding:"25px",}}>  
+                <div className={classes.leftsubheading}>Who should attend?</div>
+                <div className={classes.leftsubtext}>Our Boot Camps are designed to energize, educate &amp; empower real estate agents, brokers &amp; teams across the U.S.</div>
+                <br />
+                <div className={classes.leftsubheading}>What will we learn?</div>
+                <div className={classes.leftsubtext}>Agents will learn tips, tricks &amp; strategies in a fast-paced, easy-to-digest environment that will help them grow their online brand through intelligent, thoughtful marketing.</div>
+                <br />
+                <div className={classes.leftsubheading}>How long is the event?</div>
+                <div className={classes.leftsubtext}>Our onsite event usually runs about 2-hours, complete with breakfast and/or lunch, Q&amp;A and exciting giveaways (like an Amazon Fire Tablet to help agents stay mobile).</div>
+                </div>
+                <div className={classes.flexItem} style={{padding:"25px",}}>
+                <div className={classes.leftsubheading}>Is there a cost to participate?</div>
+                <div className={classes.leftsubtext}>No. All of our Boot Camp events are FREE for attendees.</div>
+                <br />
+                <div className={classes.leftsubheading}>How do I register?</div>
+                <div className={classes.leftsubtext}>Click here to view our upcoming tour schedule.  Select an event you’d like to attend and follow the online registration process...and be sure to invite your friends &amp; colleagues who may also be interested!</div>
+                <br />
+                <div className={classes.leftsubheading}>How do I book a boot camp for my agents?</div>
+                <div className={classes.leftsubtext}>That’s easy.  Fill out the request form above OR contact Todd Colthorp, Director of Elevate Events at XXX.XXX.XXXX | todd@tryelevate.com to start the conversation.</div>
+                </div>
+            </div>
+        </Container>
         </div>
-      </Container>
-      <Container>
-        <BootcampFaqs className={classes.faqs} faqs={faqs} />
-      </Container>
-      <BootcampSignup
-        className={classes.signup}
-        signupMessaging={signupMessaging}
-      />
-      <Container>
-        <BootcampTopics className={classes.topics} topics={topics} />
-      </Container>
-      <Container>
-        <BootcampInstructors
-          className={classes.instructors}
-          instructors={instructors}
-        />
-      </Container>
+
+
       <Footer />
     </div>
   );
@@ -193,4 +249,116 @@ export default withStyles((theme) => ({
     paddingTop: "48px",
     paddingBottom: "92px",
   },
+  ctabutton: {
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "16px",
+    textAlign: "center",
+    fontWeight: "500",
+    minWidth: "300px",
+    letterSpacing: ".25px",
+    backgroundColor: theme.colors.secondary,
+    color: "#FFF",
+    display: "block",
+    marginTop: "30px",
+    maxWidth: "300px",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "3px",
+    display: "inline-block",
+  },
+  flexContainer: {
+    padding: "0",
+    margin: '0',
+  
+    display: "-webkit-box",
+    display: "-moz-box",
+    display: "-ms-flexbox",
+    display: "-webkit-flex",
+    display: "flex",
+    flexWrap: "wrap",
+  
+  },
+
+  flexItem: {
+    maxWidth: "100%",
+    width: "100%",
+    paddingRight: "0px",
+    paddingBottom: "25px",
+  
+  
+    [theme.breakpoints[900]]: {
+        maxWidth: "50%",
+        width: "50%",
+        paddingRight: "20px",
+        paddingBottom: "0px",
+    },
+  },
+
+  leftsubtext: {
+    color: "#5A5B5C",
+    fontWeight: "500",
+    fontSize: "18px",
+    letterSpacing: ".25px",
+    lineHeight: "1.6",
+    marginbottom: "20px",
+    textAlign: "left",
+    [theme.breakpoints[900]]: {
+        fontSize: "16px",
+    },
+  },
+
+  leftheading: {
+    color: "#4bbbb1",
+    fontSize: "30px",
+    fontWeight: "700",
+    letterSpacing: ".25px",
+    fontWeight: "600",
+    lineHeight: "1.6",
+  
+    [theme.breakpoints[900]]: {
+        fontSize: "28px",
+    },
+  },
+
+  leftsubheading: {
+    color: "#4bbbb1",
+    fontSize: "18px",
+    fontWeight: "900",
+    letterSpacing: ".25px",
+    fontWeight: "600",
+    lineHeight: "1.6",
+  
+    [theme.breakpoints[900]]: {
+        fontSize: "18px",
+    },
+  },
+  centerheading: {
+    color: "#4bbbb1",
+    fontSize: "30px",
+    fontWeight: "700",
+    letterSpacing: ".25px",
+    fontWeight: "600",
+    lineHeight: "1.6",
+    textAlign: "center",
+  
+    [theme.breakpoints[900]]: {
+        fontSize: "28px",
+    },
+  },
+  centersubtext: {
+    color: "#5A5B5C",
+    fontWeight: "500",
+    fontSize: "16px",
+    letterSpacing: ".25px",
+    lineHeight: "1.6",
+    marginbottom: "20px",
+    textAlign: "center",
+  
+    [theme.breakpoints[900]]: {
+        fontSize: "16px",
+    },
+  },
+
+
 }))(Bootcamp);
