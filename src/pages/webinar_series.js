2379339b70e54a8fd9524666fe68718714736e0a
Matt Litchliter <mattlitchliter@Matts-MacBook-Pro.local>
2019-03-28 15:38:57 -0500

    site 2019 updates

--- src/pages/webinar_series.js
+++ src/pages/webinar_series.js
@@ -14,6 +14,7 @@ import WideImage from "../images/1200.jpg";
 import exImage from "../images/example.png";
 import exImage2 from "../images/example2.png";
 import exImage3 from "../images/example3.png";
+import Slidegif from "../images/slide.gif";
 import bgImage from "../images/examplebg.jpg";
 
 const Demo = ({ classes }) => {
@@ -28,37 +29,79 @@ const Demo = ({ classes }) => {
       from the comfort of your couch!
       </PageTitleImage>
 
-      <div style={{ margin: "45px", }}>
-        <Section2Col secTitle="Section Header" secImage={TestImage}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          <Link to="/upgrade" className={classes.ctabutton}>
-          Upgrade Button Text2
-                    </Link>
-                    <Link to="/upgrade" className={classes.ctabutton}>
-          Upgrade Button Text3
-                    </Link>
-        </Section2Col>
-      </div>
-    
-      <div style={{ margin: "45px", }}>
-        <Section1Col secTitle="Center Header">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        
-          <Link to="/upgrade" className={classes.ctabutton}>
-          Upgrade Button Text3
-                    </Link>
 
+
+
+
+
+
+
+
+
+      <div style={{ paddingTop:"35px", paddingLeft:"25px", paddingright:"25px",}}>
+        <div>
+        <Section1Col>
+        Can’t make one of our in-person Boot Camps? No worries!<br />
+        Join one of our upcoming webinars where we cram as much of our great education<br />
+        into a one-hour deep dive.  Business suit NOT required!
+        
+          <div style={{ textAlign: "center", }}>
+                <Link to="/upgrade" className={classes.ctabutton}>
+                  Register TODAY</Link>
+                  <br /><br />
+                <img src={Slidegif} style={{width:"100%", maxWidth:"1100px", display:"block", marginLeft:"auto", marginRight:"auto", paddingTop:"13px",}}/>
+          </div>
+          
         </Section1Col>
       </div>
-
-        <BgSection bgImage={bgImage} secTitle="Section Header" secLink="/upgrade" secButton="Upgrade Button Text" secImage={exImage}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </BgSection>
+      </div>
 
 
-        <BgSection bgImage={bgImage} secTitle="Section Header" secLink="/upgrade" secButton="Upgrade Button Text" secImage={exImage3}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </BgSection>
+
+
+
+
+
+
+        <div style={{backgroundColor:"#f7f7f7"}}><Container>
+                <div style={{textAlign:"center", paddingTop:"45px", paddingBottom:"10px",}}>
+
+                   
+                <div className={classes.centerheading}><span style={{ fontWeight: "bold" }}>Frequently Asked Questions</span></div>
+                <div className={classes.centersubtext}>about our Webinar Series</div>
+                    
+
+                </div>
+  </Container>
+  </div>
+
+
+        <div style={{paddingTop:"20px", paddingBottom:"50px", backgroundColor:"#f7f7f7"}}>
+        <Container>
+            <div className={classes.flexContainer}>
+                <div className={classes.flexItem} style={{padding:"25px",}}>  
+                <div className={classes.leftsubheading}>Who should attend?</div>
+                <div className={classes.leftsubtext}>Our Webinars are designed to energize, educate &amp; empower real estate agents across the U.S.</div>
+                <br />
+                <div className={classes.leftsubheading}>What will we learn?</div>
+                <div className={classes.leftsubtext}>Agents will learn tips, tricks &amp; strategies in a fast-paced, easy-to-digest environment that will help them grow their online brand through intelligent, thoughtful marketing.</div>
+                <br />
+                <div className={classes.leftsubheading}>How long is the event?</div>
+                <div className={classes.leftsubtext}>Our webinars run 1-hour ent usually runs about 2-hours, complete with breakfast and/or lunch, Q&amp;A and exciting giveaways (like an Amazon Fire Tablet to help agents stay mobile).</div>
+                </div>
+                <div className={classes.flexItem} style={{padding:"25px",}}>
+                <div className={classes.leftsubheading}>Is there a cost to participate?</div>
+                <div className={classes.leftsubtext}>No.  All of our Boot Camp events are FREE for attendees.</div>
+                <br />
+                <div className={classes.leftsubheading}>How do I register?</div>
+                <div className={classes.leftsubtext}>Click here to view our upcoming tour schedule. Select an event you’d like to attend and follow the online registration process...and be sure to invite your friends &amp; colleagues who may also be interested!</div>
+                <br />
+                <div className={classes.leftsubheading}>How do I book a boot camp for my agents?</div>
+                <div className={classes.leftsubtext}>That’s easy.  Fill out the request form above OR contact Todd Colthorp, Director of Elevate Events at XXX.XXX.XXXX | todd@tryelevate.com to start the conversation.</div>
+                </div>
+            </div>
+        </Container>
+        </div>
 
       <Footer />
     </div>
@@ -81,6 +124,84 @@ export default withStyles((theme) => ({
         paddingTop: "48px",
     },
   },
+  leftsubheading: {
+    color: "#4bbbb1",
+    fontSize: "18px",
+    fontWeight: "900",
+    letterSpacing: ".25px",
+    fontWeight: "600",
+    lineHeight: "1.6",
+  
+    [theme.breakpoints[900]]: {
+        fontSize: "18px",
+    },
+  },
+  leftsubtext: {
+    color: "#5A5B5C",
+    fontWeight: "500",
+    fontSize: "18px",
+    letterSpacing: ".25px",
+    lineHeight: "1.6",
+    marginbottom: "20px",
+    textAlign: "left",
+    [theme.breakpoints[900]]: {
+        fontSize: "16px",
+    },
+  },
+
+  flexContainer: {
+    padding: "0",
+    margin: '0',
+  
+    display: "-webkit-box",
+    display: "-moz-box",
+    display: "-ms-flexbox",
+    display: "-webkit-flex",
+    display: "flex",
+    flexWrap: "wrap",
+  
+  },
+
+  flexItem: {
+    maxWidth: "100%",
+    width: "100%",
+    paddingRight: "0px",
+    paddingBottom: "25px",
+  
+  
+    [theme.breakpoints[900]]: {
+        maxWidth: "50%",
+        width: "50%",
+        paddingRight: "20px",
+        paddingBottom: "0px",
+    },
+  },
+  centerheading: {
+    color: "#4bbbb1",
+    fontSize: "30px",
+    fontWeight: "700",
+    letterSpacing: ".25px",
+    fontWeight: "600",
+    lineHeight: "1.6",
+    textAlign: "center",
+  
+    [theme.breakpoints[900]]: {
+        fontSize: "28px",
+    },
+  },
+  centersubtext: {
+    color: "#5A5B5C",
+    fontWeight: "500",
+    fontSize: "16px",
+    letterSpacing: ".25px",
+    lineHeight: "1.6",
+    marginbottom: "20px",
+    textAlign: "center",
+  
+    [theme.breakpoints[900]]: {
+        fontSize: "16px",
+    },
+  },
   ctabutton: {
     alignContent: "center",
     justifyContent: "center",
@@ -91,7 +212,7 @@ export default withStyles((theme) => ({
     letterSpacing: ".25px",
     backgroundColor: theme.colors.secondary,
     color: "#FFF",
-    display: "block",
+    display: "inline-block",
     marginTop: "30px",
     maxWidth: "300px",
     textDecoration: "none",
