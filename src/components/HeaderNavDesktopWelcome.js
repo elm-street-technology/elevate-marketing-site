8252bc271d5e68fd97169d3506799835e0af8d8a
Matt Litchliter <mattlitchliter@Matts-MacBook-Pro.local>
2019-04-03 11:40:17 -0500

    Created new header/footer components

---
+++ src/components/HeaderNavDesktopWelcome.js
@@ -0,0 +1,176 @@
+import React from "react";
+import Link from "gatsby-link";
+import withStyles from "elevate-ui/withStyles";
+import classNames from "classnames";
+import ArrowDropDown from "elevate-ui-icons/ArrowDropDown";
+
+const HeaderNavDesktop = ({ classes, className }) => {
+  return (
+    <nav className={classNames(classes.nav, className)}>
+      <div className={classes.link}>
+        <div className={classes.navItem}>
+          Solutions
+          <ArrowDropDown size={20} />
+        </div>
+        <ul className={classes.dropDown}>
+          <Link className={classes.listItemLink} to="/agents">
+            <li className={classes.listItem}>Agents</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/teams">
+            <li className={classes.listItem}>Teams</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/brokers">
+            <li className={classes.listItem}>Brokers</li>
+          </Link>
+        </ul>
+      </div>
+      <div className={classes.link}>
+        <div className={classes.navItem}>
+          Products
+          <ArrowDropDown size={20} />
+        </div>
+        <ul className={classes.dropDown}>
+          <Link className={classes.listItemLink} to="/elevate-dms">
+            <li className={classes.listItem}>DMS</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/elevate-social-pro">
+            <li className={classes.listItem}>Social Pro</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/elevate-idx">
+            <li className={classes.listItem}>IDX</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/elevate-ignite">
+            <li className={classes.listItem}>Ignite</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/elevate-elite">
+            <li className={classes.listItem}>Elite</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/crm">
+            <li className={classes.listItem}>CRM</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/recruiter">
+            <li className={classes.listItem}>Recruiter</li>
+          </Link>
+        </ul>
+      </div>
+      <div className={classes.link}>
+        <div className={classes.navItem}>
+          Support
+          <ArrowDropDown size={20} />
+        </div>
+        <ul className={classes.dropDown}>
+          <a
+            className={classes.listItemLink}
+            href="https://elmstreettechnology.zendesk.com/hc/en-us"
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <li className={classes.listItem}>Help Center</li>
+          </a>
+          <Link className={classes.listItemLink} to="/webinars">
+            <li className={classes.listItem}>Webinars</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/blog">
+            <li className={classes.listItem}>Blog</li>
+          </Link>
+        </ul>
+      </div>
+      <div className={classes.link}>
+        <div className={classes.navItem}>
+          Explore
+          <ArrowDropDown size={20} />
+        </div>
+        <ul className={classes.dropDown}>
+          <Link className={classes.listItemLink} to="/bootcamp">
+            <li className={classes.listItem}>Social Media Boot Camp</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/events">
+            <li className={classes.listItem}>Upcoming Events</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/testimonials">
+            <li className={classes.listItem}>Happy Clients</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/get-started">
+            <li className={classes.listItem}>Schedule a Demo</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/markets">
+            <li className={classes.listItem}>MLS Markets</li>
+          </Link>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default withStyles((theme) => ({
+  nav: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+    marginRight: "16px",
+    alignSelf: "flex-end",
+
+    "& > * + *": {
+      marginLeft: "16px",
+    },
+  },
+  navItem: {
+    display: "flex",
+  },
+
+  link: {
+    float: "left",
+    padding: "1rem",
+    position: "relative",
+    textDecoration: "none",
+    fontSize: "18px",
+    fontWeight: "500",
+    color: "#64616E",
+    display: "block",
+
+    "&:hover": {
+      cursor: "pointer",
+    },
+
+    "&:hover $dropDown": {
+      visibility: "visible",
+      opacity: "1",
+      zIndex: "999",
+      transitionDelay: "0s, 0s, .3s",
+    },
+  },
+  listItem: {
+    clear: "both",
+    padding: "20px",
+    width: "100%",
+    fontSize: "15px",
+    borderTop: "1px solid rgba(236,236,236, .6)",
+
+    "&:hover": {
+      color: "#2E2E35",
+      background: "rgba(236,236,236, .4)",
+    },
+  },
+  listItemLink: {
+    color: "#64616E",
+    textDecoration: "none",
+  },
+  dropDown: {
+    background: "#FFF",
+    borderRadius: "6px",
+    boxShadow: "0 1px 3px 0 rgba(0,0,0,.05), 0 5px 20px 0 rgba(0,0,0,.15)",
+    listStyle: "none",
+    minWidth: "220px",
+    margin: "0",
+    paddingLeft: "0px",
+    visibility: "hidden",
+    opacity: "0",
+    position: "absolute",
+    marginTop: "1rem",
+    left: "0",
+    transform: "translateY(0%)",
+    transition: "all .3s ease-in-out",
+    overflow: "hidden",
+    zIndex: "999",
+  },
+}))(HeaderNavDesktop);
---
+++ src/components/HeaderNavMobileWelcome.js
@@ -0,0 +1,183 @@
+import React from "react";
+import Link from "gatsby-link";
+import withStyles from "elevate-ui/withStyles";
+import classNames from "classnames";
+import Close from "elevate-ui-icons/Close";
+
+const HeaderNavMobile = ({ classes, className, onMenuClose }) => {
+  return (
+    <nav className={classNames(classes.nav, className)}>
+      <div className={classes.topNav}>
+        <button onClick={onMenuClose} className={classes.navMobileIcon}>
+          <Close size={30} aria-hidden={"true"} />
+        </button>
+      </div>
+      <div className={classes.link}>
+        <div className={classes.navItem}>Solutions</div>
+        <ul className={classes.dropDown}>
+          <Link className={classes.listItemLink} to="/agents">
+            <li className={classes.listItem}>Agents</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/teams">
+            <li className={classes.listItem}>Teams</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/brokers">
+            <li className={classes.listItem}>Brokers</li>
+          </Link>
+        </ul>
+      </div>
+      <div className={classes.link}>
+        <div className={classes.navItem}>Products</div>
+        <ul className={classes.dropDown}>
+          <Link className={classes.listItemLink} to="/elevate-dms">
+            <li className={classes.listItem}>DMS</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/elevate-social-pro">
+            <li className={classes.listItem}>Social Pro</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/elevate-idx">
+            <li className={classes.listItem}>IDX</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/elevate-ignite">
+            <li className={classes.listItem}>Ignite</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/elevate-elite">
+            <li className={classes.listItem}>Elite</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/crm">
+            <li className={classes.listItem}>CRM</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/recruiter">
+            <li className={classes.listItem}>Recruiter</li>
+          </Link>
+        </ul>
+      </div>
+      <div className={classes.link}>
+        <div className={classes.navItem}>Support</div>
+        <ul className={classes.dropDown}>
+          <a
+            className={classes.listItemLink}
+            href="https://elmstreettechnology.zendesk.com/hc/en-us"
+            target="_blank"
+          >
+            <li className={classes.listItem}>Help Center</li>
+          </a>
+          <Link className={classes.listItemLink} to="/webinars">
+            <li className={classes.listItem}>Webinars</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/blog">
+            <li className={classes.listItem}>Blog</li>
+          </Link>
+        </ul>
+      </div>
+
+      <div className={classes.link}>
+        <div className={classes.navItem}>Explore</div>
+        <ul className={classes.dropDown}>
+          <Link className={classes.listItemLink} to="/bootcamp">
+            <li className={classes.listItem}>Social Media Boot Camp</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/events">
+            <li className={classes.listItem}>Upcoming Events</li>
+          </Link>
+
+          <Link className={classes.listItemLink} to="/testimonials">
+            <li className={classes.listItem}>Happy Clients</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/get-started">
+            <li className={classes.listItem}>Schedule a Demo</li>
+          </Link>
+          <Link className={classes.listItemLink} to="/markets">
+            <li className={classes.listItem}>MLS Markets</li>
+          </Link>
+        </ul>
+      </div>
+      <Link to="/get-started" className={classes.getStartedLink}>
+        Get Started
+      </Link>
+    </nav>
+  );
+};
+
+export default withStyles((theme) => ({
+  nav: {
+    display: "flex",
+    flexDirection: "column",
+    maxWidth: "280px",
+    width: "100%",
+    paddingTop: "30px",
+    paddingBottom: "30px",
+    position: "fixed",
+    top: "0",
+    left: "0",
+    right: "0",
+    bottom: "0",
+    background: "#FFF",
+    zIndex: "9999999",
+    overflowY: "scroll",
+    boxShadow: "0 1px 3px 0 rgba(0,0,0,.1), 0 5px 20px 0 rgba(0,0,0,.2)",
+  },
+  topNav: {
+    display: "flex",
+    alignItems: "center",
+  },
+  navItem: {
+    display: "flex",
+    fontWeight: "600",
+    fontSize: "16px",
+  },
+
+  navMobileIcon: {
+    color: "#2E2E35",
+    position: "fixed",
+    display: "inline-flex",
+    top: "26px",
+    right: "26px",
+    zIndex: "1",
+
+    "&:focus": {
+      outline: "1px solid #fff",
+    },
+  },
+
+  link: {
+    paddingLeft: "20px",
+    padding: "1rem",
+    fontSize: "18px",
+    color: "#64616E",
+    borderBottom: "1px solid rgba(236,236,236, .6)",
+  },
+  listItem: {
+    padding: "15px",
+    width: "100%",
+    fontSize: "14px",
+  },
+  listItemLink: {
+    color: "#64616E",
+    textDecoration: "none",
+  },
+  dropDown: {
+    background: "#FFF",
+    display: "flex",
+    flexDirection: "column",
+    marginTop: "1rem",
+  },
+
+  getStartedLink: {
+    display: "flex",
+    flexShrink: "0",
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: "transparent",
+    fontSize: "14px",
+    fontWeight: "700",
+    letterSpacing: ".25px",
+    color: theme.colors.secondary,
+    border: `1px solid ${theme.colors.secondary}`,
+    textDecoration: "none",
+    padding: "12px 16px",
+    borderRadius: "4px",
+    alignSelf: "center",
+    marginTop: "20px",
+  },
+}))(HeaderNavMobile);
