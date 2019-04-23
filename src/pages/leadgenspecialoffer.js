7cc70785efa13f14a7178b16e58ce1bc1e217af2
Matt Litchliter <mattlitchliter@Matts-MacBook-Pro.local>
2019-04-23 10:00:47 -0500

    Lead Gen Special Offer

---
+++ src/pages/leadgenspecialoffer.js
@@ -0,0 +1,577 @@
+import React from "react";
+import Link from "gatsby-link";
+import withStyles from "elevate-ui/withStyles";
+import Container from "../components/Container";
+import SEO from "../components/SEO";
+import Logo from "../components/Logo";
+import Hr from "elevate-ui/Hr";
+import WistiaVideo from "../components/WistiaVideo";
+import Chat from "elevate-ui-icons/Chat";
+import QuestionAnswer from "elevate-ui-icons/QuestionAnswer";
+import ViewCarousel from "elevate-ui-icons/ViewCarousel";
+import PlayCircleFilled from "elevate-ui-icons/PlayCircleFilled";
+import CheckCircle from "elevate-ui-icons/CheckCircle";
+import SMBCLogo from "../images/smbcLogo.jpg";
+import Elite from "../images/elite.jpg";
+import Social_pro from "../images/social_pro.jpg";
+import Dms from "../images/dms.jpg";
+import Ignite from "../images/ignite.jpg";
+import Pro from "../images/pro.jpg";
+import Tour from "../images/tour_icon.png";
+import Web from "../images/webinar_icon.png";
+import Social from "../images/social_pic.jpg";
+import Leadpic from "../images/leadpic.jpg";
+import Email from "../images/email.jpg";
+import Star from "../images/star.png";
+import Encore from "../images/encore_icon.png";
+import Emails from "../images/6emails_pic.png";
+import Dose from "../images/dose_pic.png";
+import Line from "../images/line.png";
+import Freebg from "../images/free_bg.jpg";
+const leadgenspecialoffer = ({ classes }) => {
+
+  return (
+    <div className={classes.root}>
+      <SEO />
+    
+      <Container>
+        <div className={classes.top}>
+          <div className={classes.centerHeading} style={{paddingBottom:"35px"}}>
+            <div className={classes.heading}><strong>A Special Offer from Elevate</strong></div>
+            <div className={classes.bold}>Expires Friday, April 26th at 6pm PST.
+            
+</div>
+          </div>
+        </div>
+      </Container>
+
+
+   
+      <Container>
+      
+      
+        <div className={classes.top}>
+        <div className={classes.infoboxGray2}>
+          <div className={classes.column3} style={{ textAlign: "center", paddingBottom: "35px", }}>
+          <div style={{ textAlign: "center",paddingBottom:"20px" }}>
+          <img src={Ignite} style={{ width:"100%", maxWidth:"200px"}}/>
+        </div>
+            
+                <div className={classes.productCol} style={{ textAlign: "left", paddingLeft:"10px", paddingRight:"10px" }}>
+                <div className={classes.bold} style={{color:"#fb9806"}}>• PAY-PER-CLICK<br />
+                LEAD GENERATION</div>
+                <span style={{color:"#fb9806", fontSize:"14px", lineHeight:"16px"}}>(Google, Bing, Yahoo)</span>
+                <div className={classes.bold} style={{color:"#fb9806"}}>• ADVANCED CRM</div>
+                <div className={classes.bold} style={{color:"#fb9806"}}>• CMA WIZARD</div>
+                </div>
+
+                <div>
+                <img src={Line} style={{ width:"100%", maxWidth:"200px"}}/>
+                <br />
+                  <div className={classes.bold} style={{color:"#fb9806", textAlign: "center"}}>6 MONTH AGREEMENT
+</div>
+<br />
+<div style={{color:"#fb9806", textAlign: "center",}}>
+<div className={classes.bold} style={{color:"#fb9806", paddingTop:"28px"}}>MONTHLY</div>
+<div style={{color:"#fb9806",}}><span style={{textDecoration:"line-through"}}>$699/m</span> <span style={{color:"#f97306"}} className={classes.bold}>$549/m</span></div>
+<div>One Time Set-Up: <span style={{textDecoration:"line-through"}}>$700</span> <span style={{color:"#f97306"}} className={classes.bold}>$200</span></div>
+</div>
+              
+               
+                <a target="_blank" href="https://elmstreettechnology.recurly.com/subscribe/ignite" className={classes.getStartedLinkignite}>
+                  Subscribe NOW
+                    </a>
+                    
+                   
+                    </div>
+
+            </div>
+            </div>
+            <div className={classes.infoboxGray2}>
+          <div className={classes.column3} style={{ textAlign: "center", paddingBottom: "35px" }}>
+          
+          <div style={{ textAlign: "center",paddingBottom:"20px" }}>
+          <img src={Elite} style={{ width:"100%", maxWidth:"200px"}}/>
+        </div>
+                <div className={classes.productCol} style={{ textAlign: "left", paddingLeft:"10px", paddingRight:"10px"}}>
+                <div className={classes.bold} style={{color:"#EE5A55"}}>• EMAIL MARKETING CAMPAIGNS</div>
+                <div className={classes.bold} style={{color:"#EE5A55"}}>• IDX WEBSITE</div>
+                <div className={classes.bold} style={{color:"#EE5A55"}}>• BLOG</div>
+                <div className={classes.bold} style={{color:"#EE5A55"}}>• SOCIAL MEDIA SERVICES</div>
+                <div className={classes.bold} style={{color:"#fb9806"}}>• PAY-PER-CLICK<br />
+                LEAD GENERATION</div>
+                <span style={{color:"#fb9806", fontSize:"14px", lineHeight:"16px"}}>(Google, Bing, Yahoo)</span>
+                <div className={classes.bold} style={{color:"#fb9806"}}>• ADVANCED CRM</div>
+                <div className={classes.bold} style={{color:"#fb9806"}}>• CMA WIZARD</div>
+                </div>
+
+                <div>
+                <img src={Line} style={{ width:"100%", maxWidth:"200px"}}/>
+                <br />
+                <span className={classes.bold} style={{color:"#EE5A55"}}>6 MONTH AGREEMENT</span>
+                <br />
+                  <br />
+                  <div className={classes.bold} style={{color:"#EE5A55", paddingTop:"28px"}}>MONTHLY</div>
+<div style={{color:"#EE5A55",}}><span style={{textDecoration:"line-through"}}>$899/m</span> <span style={{color:"#f97306"}} className={classes.bold}>$749/m</span></div>
+<div style={{color:"#EE5A55",}}>One Time Set-Up: <span style={{textDecoration:"line-through"}}>$800</span> <span style={{color:"#f97306"}} className={classes.bold}>$500</span></div>
+
+
+                                  <a target="_blank" href="https://elmstreettechnology.recurly.com/subscribe/elevateelite" className={classes.getStartedLinkelite}>
+                                    Subscribe NOW
+                                      </a>
+
+                                      
+                  </div>
+
+
+
+          </div>
+          </div>
+
+          <div className={classes.infoboxGray2}>
+          <div className={classes.column3} style={{ textAlign: "center", paddingBottom: "35px" }}>
+          <div style={{ textAlign: "center",paddingBottom:"20px" }}>
+          <img src={Pro} style={{ width:"100%", maxWidth:"200px"}}/>
+        </div>
+                <div className={classes.productCol} style={{ textAlign: "left", paddingLeft:"10px", paddingRight:"10px"}}> 
+                <div className={classes.bold} style={{color:"#514392"}}>• LEAD SCRUBBING CONCIERGE</div>
+                <div className={classes.bold} style={{color:"#EE5A55"}}>• EMAIL MARKETING CAMPAIGNS</div>
+                <div className={classes.bold} style={{color:"#EE5A55"}}>• IDX WEBSITE</div>
+                <div className={classes.bold} style={{color:"#EE5A55"}}>• BLOG</div>
+                <div className={classes.bold} style={{color:"#EE5A55"}}>• SOCIAL MEDIA SERVICES</div>
+                <div className={classes.bold} style={{color:"#fb9806"}}>• PAY-PER-CLICK<br />
+                LEAD GENERATION</div>
+                <span style={{color:"#fb9806", fontSize:"14px", lineHeight:"16px"}}>(Google, Bing, Yahoo)</span>
+                <div className={classes.bold} style={{color:"#fb9806"}}>• ADVANCED CRM</div>
+                <div className={classes.bold} style={{color:"#fb9806"}}>• CMA WIZARD</div>
+                
+                </div>
+
+
+                <div>
+                <img src={Line} style={{ width:"100%", maxWidth:"200px"}}/>
+                <br />
+                <span className={classes.bold} style={{color:"#514392"}}>6 MONTH AGREEMENT</span>
+                <br /><br />
+
+
+                <div className={classes.bold} style={{color:"#514392", paddingTop:"28px"}}>MONTHLY</div>
+<div style={{color:"#514392",}}><span style={{textDecoration:"line-through"}}>$1199/m</span> <span style={{color:"#f97306"}} className={classes.bold}>$1049/m</span></div>
+<div style={{color:"#514392",}}>One Time Set-Up: <span style={{textDecoration:"line-through"}}>$800</span> <span style={{color:"#f97306"}} className={classes.bold}>$500</span></div>
+
+                
+                <a target="_blank" href="https://elmstreettechnology.recurly.com/subscribe/elevateelitepro" className={classes.getStartedLinkpro}>
+                  Subscribe NOW
+                    </a>
+                  
+                </div>
+                
+          </div>
+          </div>
+
+
+          </div>
+      </Container>
+
+
+<div style={{ textAlign: "center", padding: "35px", fontSize:"12px", lineHeight:"18px" }}>
+Sales tax charged where applicable.  Please note that setup fees are non-refundable.  <a target="_blank" href="http://tryelevate.com/terms-of-service/" >Full Terms of Service</a><br /><br />
+</div>
+
+
+
+<div style={{backgroundColor:"#fafafa",}}>
+      <Container>
+            <div className={classes.section}></div>
+        <div className={classes.top}>
+          <div className={classes.centerHeading} style={{paddingBottom:"55px",}}>
+            <div className={classes.heading}>
+            Trying to decide which Elevate solution is right for you?
+            </div>
+            Schedule 15-minutes of one-on-one time with an Elevate coach.
+            <br />
+            <br />
+            <div dangerouslySetInnerHTML={{
+            __html: '<iframe style="height:740px" src="https://calendly.com/estsean/15min-1?embed_domain=&amp;embed_type=Inline" width="100%" height="100%" frameborder="0"></iframe>',
+          }}>
+            </div>
+          </div>
+
+          </div>
+          </Container>
+        </div>
+
+
+
+
+
+
+
+
+<div style={{backgroundColor:"gray",backgroundImage:"url("+Freebg+")"}}>
+      <Container>
+            <div className={classes.section}></div>
+        <div className={classes.top}>
+          <div className={classes.centerHeading} style={{paddingBottom:"55px",}}>
+            <div className={classes.heading} style={{color:"#ffffff"}}>
+            FREE Marketing Goodies
+            </div>
+            Keep the learning going!
+          </div>
+        </div>
+      
+      
+        <div className={classes.top}>
+          <div className={classes.column3a} style={{ textAlign: "center", paddingBottom: "35px" }}>
+          <div style={{ textAlign: "center",paddingBottom:"20px" }}>
+          <img src={Dose} style={{ width:"100%", maxWidth:"300px"}}/>
+        </div>
+            
+          <div className={classes.heading2} style={{color:"#ffffff"}}>Daily Marketing Tips<br />
+free daily email
+</div>
+                <div> 
+                Sign up to receive your daily dose<br />
+                of marketing awesomeness<br />
+                delivered straight to your inbox.
+                </div>
+                <a target="_blank" href="http://3sixtyfive.agency/" className={classes.getStartedLink2}>
+                  Register Today
+                    </a>
+                
+
+            </div>
+          <div className={classes.column3a} style={{ textAlign: "center", paddingBottom: "35px" }}>
+          <div style={{ textAlign: "center",paddingBottom:"20px" }}>
+          <img src={Emails} style={{ width:"100%", maxWidth:"300px"}}/>
+        </div>
+          <div className={classes.heading2} style={{color:"#ffffff"}}>Email Marketing Tips<br />
+free download
+</div>
+                <div> 
+                6 emails every real estate agent<br />
+                should be sending to nurture past,<br />
+                present &amp; future clients.
+                </div>
+                <a target="_blank" href="/assets/6emails_elevate.pdf" className={classes.getStartedLink2}>
+                  Download PDF
+                    </a>
+          </div>
+
+          <div className={classes.column3a} style={{ textAlign: "center", paddingBottom: "35px" }}>
+          <div style={{ textAlign: "center",paddingBottom:"20px" }}>
+          <img src={Leadpic} style={{ width:"100%", maxWidth:"300px"}}/>
+        </div>
+          <div className={classes.heading2} style={{color:"#ffffff"}}>Lead Generation<br />
+Boot Camp Basics
+</div>
+                <div> 
+                Quick tips, tricks<br />
+                &amp; takeaways from our<br />
+                Lead Generation Boot Camp Webinar.
+                </div>
+                <a target="_blank" href="/assets/lead_gen_basics.pdf" className={classes.getStartedLink2}>
+                  Download PDF
+                    </a>
+          </div>
+
+
+
+          </div>
+      </Container>
+      </div>
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+  
+
+
+
+  );
+};
+
+
+export default withStyles((theme) => ({
+  root: {
+    position: "relative",
+    paddingTop: "96px",
+    paddingBottom: "0px",
+    lineHeight: "1.75em",
+  },
+  top: {
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "top",
+  },
+  section:{
+    paddingTop: "60px",
+  },
+  heading: {
+    color: theme.colors.secondary,
+    fontSize: "32px",
+    lineHeight: "40px",
+    fontWeight: "700",
+    marginRight: "auto",
+    paddingBottom: "20px",
+  },
+  heading2: {
+    color: theme.colors.secondary,
+    fontSize: "22px",
+    fontWeight: "700",
+    marginRight: "auto",
+    paddingBottom: "20px",
+  },
+  centerHeading:{
+    alignItems: "center",
+    width: "100%",
+    textAlign: "center",
+  },
+  bold:{
+    fontWeight:"700",
+  },
+  column:{
+    width:"525px",
+    maxWidth: "100%",
+    paddingRight:"18px",
+    paddingLeft:"18px",
+    verticalAlign: "top",
+  },
+  
+  column2:{
+    width:"710px",
+    maxWidth: "100%",
+    paddingRight:"10px",
+    paddingLeft:"10px",
+    verticalAlign: "top",
+  },
+
+  column3:{
+    width:"300px",
+    maxWidth: "100%",
+    paddingRight:"10px",
+    paddingLeft:"10px",
+    verticalAlign: "top",
+  },
+
+  column3a:{
+    width:"350px",
+    maxWidth: "100%",
+    paddingRight:"10px",
+    paddingLeft:"10px",
+    verticalAlign: "top",
+  },
+
+  getStartedLink: {
+    minWidth: "300px",
+    maxWidth: "300px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: theme.colors.secondary,
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "14px 16px",
+    borderRadius: "4px",
+    margin: "16px auto",
+    marginBottom: "20px",
+  },
+  getStartedLinksp: {
+    minWidth: "200px",
+    maxWidth: "200px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#56595B",
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "4px",
+    margin: "14px auto",
+    marginBottom: "20px",
+  },
+  getStartedLinkdms: {
+    minWidth: "200px",
+    maxWidth: "200px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#5BC1B8",
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "4px",
+    margin: "14px auto",
+    marginBottom: "20px",
+  },
+  getStartedLinkignite: {
+    minWidth: "200px",
+    maxWidth: "200px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#f99406",
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "4px",
+    margin: "14px auto",
+    marginBottom: "20px",
+  },
+  getStartedLinkelite: {
+    minWidth: "200px",
+    maxWidth: "200px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#EE5A55",
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "4px",
+    margin: "14px auto",
+    marginBottom: "20px",
+  },
+  getStartedLinkpro: {
+    minWidth: "200px",
+    maxWidth: "200px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#524493",
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "4px",
+    margin: "14px auto",
+    marginBottom: "20px",
+  },
+
+  getStartedLink2: {
+    minWidth: "300px",
+    maxWidth: "300px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#ffffff",
+    color: "#5dc4ba",
+    textDecoration: "none",
+    padding: "14px 16px",
+    borderRadius: "4px",
+    margin: "16px auto",
+    marginBottom: "20px",
+  },
+  darkTealLink: {
+    width: "250px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    letterSpacing: ".25px",
+    backgroundColor: theme.colors.secondaryDark,
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "14px 16px",
+    borderRadius: "4px",
+    margin: "16px auto",
+  },
+  grayLink: {
+    width: "250px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    letterSpacing: ".25px",
+    backgroundColor: theme.colors.gray400,
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "14px 16px",
+    borderRadius: "4px",
+    margin: "16px auto",
+  },
+  grayText:{
+    color: theme.colors.gray800,
+  },
+  infoboxGray: {
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: "#1f194c",
+    color: theme.colors.gray800,
+    textDecoration: "none",
+    padding: "40px 25px 25px 25px",
+    borderRadius: "4px",
+    margin: "30px auto",
+    textAlign: "center",
+  },
+  infoboxGray2: {
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: "#fafafa",
+    color: theme.colors.gray800,
+    textDecoration: "none",
+    padding: "30px 15px 15px 15px",
+    borderRadius: "4px",
+    margin: "30px auto",
+    textAlign: "center",
+  },
+  infoboxTeal: {
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: theme.colors.secondary,
+    color: theme.colors.gray800,
+    textDecoration: "none",
+    padding: "40px 25px 25px 25px",
+    borderRadius: "4px",
+    margin: "30px auto",
+    textAlign: "center",
+  },
+  videoBorder: {
+    borderStyle:"solid",
+    borderWidth: "1px",
+    borderColor: theme.colors.gray400,
+  },
+  iconPosition:{
+    position: "relative",
+    top:"12px",
+    right:"4px"
+  },
+  productCol: {
+    minHeight: "160px",
+
+    [theme.breakpoints[900]]: {
+      minHeight: "350px",
+    },
+  },
+}))(leadgenspecialoffer);
