3816588ba72f8cfceec7fc811e4c26c9897a9e76
Kevin Blank <kblank@gmail.com>
2019-05-08 10:24:32 -0500

    New form for CRM (#104)

---
+++ src/pages/crm.js
@@ -0,0 +1,69 @@
+import React from "react";
+import Helmet from "react-helmet";
+import withStyles from "elevate-ui/withStyles";
+import config from "../utils/siteConfig";
+import Container from "../components/Container";
+import FormCRM from "../components/FormCRM";
+import SEO from "../components/SEO";
+import LandingHeader from "../images/crm_top_image.jpg";
+import LandingFooter from "../images/crm_bottom_image.jpg";
+
+const Crm = ({ children, classes, data, tags }) => {
+  const postNode = {
+      title: `Your Single Source Solution - ${config.siteTitle}`,
+  };
+
+  return (
+    <div className={classes.root}>
+      <Helmet>
+              <title>{`Your Single Source Solution - ${config.siteTitle}`}</title>
+      </Helmet>
+      <SEO postNode={postNode} pagePath="contact" customTitle />
+
+      <Container>
+        <div >
+                  <div style={{ maxWidth: "900px", backgroundColor: "#c9ced1", marginRight:"auto",marginLeft:"auto"}}>
+                <div className={classes.landingHeaderStyle}>
+                    <img src={LandingHeader} style={{width:"100%"}} />
+                </div>
+                <div style={{ paddingLeft: "10px", paddingRight: "10px"}}>
+                    <FormCRM />
+                </div>
+                <div>
+                    <img src={LandingFooter} style={{ width: "100%" }} />
+                </div>
+            </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default withStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    padding: "0 8px",
+    margin: "70px auto",
+  },
+  heading: {
+    fontSize: "28px",
+    fontWeight: "800",
+    letterSpacing: ".14px",
+    color: "#2E2E35",
+    textAlign: "center",
+    marginBottom: "12px",
+  },
+
+  subHeading: {
+    fontSize: "18px",
+    lineHeight: "28px",
+    letterSpacing: ".08px",
+    textAlign: "center",
+    margin: "0 auto 36px",
+  },
+  landingHeaderStyle:{
+    textAlign:"center",
+  },
+}))(Crm);
