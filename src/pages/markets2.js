5d23821220e45e70976fafbf06b20762aba57683
Kevin Blank <kevinblank@Kevins-MacBook-Pro.local>
2019-10-18 14:04:26 -0500

    Moving MLS/styles

--- src/pages/markets2.js
+++ src/pages/markets2.js
@@ -24,6 +24,11 @@ const Markets2 = ({ children, classes, data, tags }) => {
   const postNode = {
       title: `Your Single Source Solution - ${config.siteTitle}`,
   };
+  const markets = data.allContentfulMarket.edges;
+  let marketCount = 0;
+  markets.forEach(({ node: market }) => {
+    marketCount += market.mlsName.length;
+  });
 
   return (
 <div className={classes.root}>
@@ -55,12 +60,64 @@ const Markets2 = ({ children, classes, data, tags }) => {
                             className={classes.mlsHeatMap}
                             />
                  </div>
+                 <div className={classes.table}>
+                    <div className={classes.tableThead}>
+                      <div className={classes.tableTh}>State</div>
+                      <div className={classes.tableTh}>MLS</div>
+                    </div>
+                    <div className={classes.tableTbody}>
+                      {markets.map(({ node: market }) => {
+                        return (
+                          <div key={market.id} className={classes.tableTr}>
+                            <div className={classes.tableTd}>{market.state}</div>
+                            <div className={classes.tableTd}>
+                              {market.mlsName &&
+                                market.mlsName.map(
+                                  (mls, i) =>
+                                    i === market.mlsName.length - 1 ? (
+                                      <span key={mls} className={classes.mls}>
+                                        {mls}
+                                      </span>
+                                    ) : (
+                                      <span key={mls}>
+                                        <span className={classes.mls}>{mls}</span>
+                                        <span
+                                          style={{
+                                            color: "#64616E",
+                                            padding: "0px 10px",
+                                          }}
+                                        >
+                                          |
+                                        </span>
+                                      </span>
+                                    )
+                                )}
+                            </div>
+                          </div>
+                        );
+                      })}
+                    </div>
+                  </div>
          </Container>
 </div>
 
 </div>
   );
 };
+
+export const query = graphql`
+  query market2Query {
+    allContentfulMarket(limit: 1000, sort: { fields: [state], order: ASC }) {
+      edges {
+        node {
+          id
+          state
+          mlsName
+        }
+      }
+    }
+  }
+`;
 
 export default withStyles((theme) => ({
   root: {
@@ -192,5 +249,45 @@ export default withStyles((theme) => ({
         paddingRight: "20px",
         paddingBottom: "0px",
     },
+  },
+  table: {
+    paddingTop: "62px",
+    width: "100%",
+  },
+  tableThead: {
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    background: "#ececec",
+  },
+  tableTh: {
+    minWidth: "120px",
+    padding: "14px",
+    fontSize: "14px",
+    textTransform: "uppercase",
+    fontWeight: "700",
+    color: "#64616E",
+  },
+  tableTbody: {
+    fontSize: "16px",
+  },
+  tableTr: {
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    borderBottom: "1px solid rgba(100, 97, 110, .25)",
+  },
+  tableTd: {
+    minWidth: "120px",
+    padding: "24px 14px",
+    fontWeight: "600",
+    maxWidth: "100%",
+    display: "flex",
+    flexWrap: "wrap",
+  },
+  mls: {
+    fontWeight: "600",
+    lineHeight: "1.6",
+    color: theme.colors.primary,
   },
 }))(Markets2);
