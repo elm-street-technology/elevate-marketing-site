3140a1aca03339299c2b16cf42ccca9225afb2c1
Matt Litchliter <mattlitchliter@Matts-MacBook-Pro.local>
2020-03-13 13:13:57 -0500

    news subscribe page

---
+++ src/pages/subscribe_today.js
@@ -0,0 +1,489 @@
+import React from "react";
+import Link from "gatsby-link";
+import withStyles from "elevate-ui/withStyles";
+import Container from "../components/Container";
+import SEO from "../components/SEO";
+import Logo from "../components/Logo";
+import FormAppointment from "../components/FormAppointment";
+import Hr from "elevate-ui/Hr";
+import WistiaVideo from "../components/WistiaVideo";
+import Chat from "elevate-ui-icons/Chat";
+import QuestionAnswer from "elevate-ui-icons/QuestionAnswer";
+import ViewCarousel from "elevate-ui-icons/ViewCarousel";
+import PlayCircleFilled from "elevate-ui-icons/PlayCircleFilled";
+import CheckCircle from "elevate-ui-icons/CheckCircle";
+import SMBCLogo from "../images/smbcLogo.jpg";
+import Elite from "../images/elite_logo.png";
+import Sp from "../images/sp_logo.png";
+import Dms from "../images/dms_logo.png";
+import Ignite from "../images/ignite.jpg";
+import Pro from "../images/pro.jpg";
+import Tour from "../images/tour_icon.png";
+import Web from "../images/webinar_icon.png";
+import Social from "../images/social_pic.jpg";
+import Leadpic from "../images/leadpic.jpg";
+import Email from "../images/email.jpg";
+import Star from "../images/star.png";
+import Encore from "../images/encore_icon.png";
+import Emails from "../images/6emails_pic.png";
+import Dose from "../images/dose_pic.png";
+import Line from "../images/line.png";
+import Freebg from "../images/free_bg.jpg";
+import Elevate_logo from "../images/elevate_logo.png";
+
+const subscribe_today = ({ classes }) => {
+
+  return (
+    <div className={classes.root}>
+      <SEO />
+    
+
+
+
+   
+      <Container>
+      
+      
+        <div className={classes.top}>
+        <div className={classes.infoboxGray2}>
+          <div className={classes.column3} style={{ textAlign: "center", paddingBottom: "35px", }}>
+          <div style={{ textAlign: "center",paddingBottom:"20px" }}>
+          <img src={Sp} style={{ width:"100%", maxWidth:"260px"}}/>
+        </div>
+            
+                <div className={classes.productCol} style={{ textAlign: "left", paddingLeft:"10px", paddingRight:"10px" }}>
+                <div className={classes.bold} style={{color:"#57585b"}}>BLOG</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>SOCIAL MEDIA SERVICES</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>MLS Feed Listing Pushes:</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• Facebook</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• LinkedIn</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• Twitter</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• YouTube</div>
+                </div>
+
+                <div>
+                <img src={Line} style={{ width:"100%", maxWidth:"200px"}}/>
+                <br />
+                  <div className={classes.bold} style={{color:"#57585b", textAlign: "center"}}>ANNUAL AGREEMENT<br />
+                  W/ FREE MONTH
+</div>
+<br />
+<div style={{color:"#57585b", textAlign: "center",}}>
+<div className={classes.bold} style={{color:"#57585b", paddingTop:"28px"}}>Monthly: <span style={{color:"#f97306"}} className={classes.bold}>$80/m</span></div>
+<div className={classes.bold}>One Time Set-Up: <span style={{textDecoration:"line-through"}}>$300</span> <span style={{color:"#f97306"}} className={classes.bold}>$100</span></div>
+<div className={classes.bold}>Annual Pre-Pay: <span style={{color:"#f97306"}} className={classes.bold}>$864</span></div>
+</div>
+              
+               
+                <a target="_blank" href="" className={classes.getStartedLinkignite}>
+                  SUBSCRIBE NOW
+                    </a>
+                    
+                   
+                    </div>
+
+            </div>
+            </div>
+            <div className={classes.infoboxGray2}>
+          <div className={classes.column3} style={{ textAlign: "center", paddingBottom: "35px" }}>
+          
+          <div style={{ textAlign: "center",paddingBottom:"20px" }}>
+          <img src={Dms} style={{ width:"100%", maxWidth:"105px"}}/>
+        </div>
+                <div className={classes.productCol} style={{ textAlign: "left", paddingLeft:"10px", paddingRight:"10px"}}>
+                <div className={classes.bold} style={{color:"#4ab8ac"}}>IDX WEBSITE</div>
+                <div className={classes.bold} style={{color:"#4ab8ac"}}>ADV. PRODUCTIVITY CRM</div>
+                <div className={classes.bold} style={{color:"#4ab8ac"}}>CMA WIZARD</div>
+                <div className={classes.bold} style={{color:"#4ab8ac"}}>EMAIL MARKETING CAMPAIGNS</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>BLOG</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>SOCIAL MEDIA SERVICES</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>MLS Feed Listing Pushes:</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• Facebook</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• LinkedIn</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• Twitter</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• YouTube</div>
+                </div>
+
+                <div>
+                <img src={Line} style={{ width:"100%", maxWidth:"200px"}}/>
+                <br />
+                <span className={classes.bold} style={{color:"#4ab8ac"}}>ANNUAL AGREEMENT<br />
+                  W/ FREE MONTH</span>
+                <br />
+                  <br />
+                  <div className={classes.bold} style={{color:"#4ab8ac", paddingTop:"28px"}}>Monthly: <span style={{textDecoration:"line-through"}}>$199/m</span> <span style={{color:"#f97306"}} className={classes.bold}>$150/m</span></div>
+<div className={classes.bold} style={{color:"#4ab8ac",}}>One Time Set-Up: <span style={{textDecoration:"line-through"}}>$700</span> <span style={{color:"#f97306"}} className={classes.bold}>$200</span></div>
+<div className={classes.bold} style={{color:"#4ab8ac",}}>Annual Pre-Pay: <span style={{textDecoration:"line-through"}}>$2149</span> <span style={{color:"#f97306"}} className={classes.bold}>$1620</span></div>
+
+<a target="_blank" href="" className={classes.getStartedLinkignite}>
+                  SUBSCRIBE NOW
+                    </a>
+
+                                      
+                  </div>
+
+
+
+          </div>
+          </div>
+
+          <div className={classes.infoboxGray2}>
+          <div className={classes.column3} style={{ textAlign: "center", paddingBottom: "35px" }}>
+          <div style={{ textAlign: "center",paddingBottom:"20px" }}>
+          <img src={Elite} style={{ width:"100%", maxWidth:"120px"}}/>
+        </div>
+                <div className={classes.productCol} style={{ textAlign: "left", paddingLeft:"10px", paddingRight:"10px", paddingBottom:"20px"}}>
+                <div className={classes.bold} style={{color:"#f15953"}}>LEAD GENERATION</div>
+                <div className={classes.bold} style={{color:"#f15953"}}>• Pay-Per-Click Campaigns</div>
+                <div className={classes.bold} style={{color:"#f15953"}}>• Facebook Ads</div>
+                <div className={classes.bold} style={{color:"#4ab8ac"}}>IDX WEBSITE</div>
+                <div className={classes.bold} style={{color:"#4ab8ac"}}>ADV. PRODUCTIVITY CRM</div>
+                <div className={classes.bold} style={{color:"#4ab8ac"}}>CMA WIZARD</div>
+                <div className={classes.bold} style={{color:"#4ab8ac"}}>EMAIL MARKETING CAMPAIGNS</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>BLOG</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>SOCIAL MEDIA SERVICES</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>MLS Feed Listing Pushes:</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• Facebook</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• LinkedIn</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• Twitter</div>
+                <div className={classes.bold} style={{color:"#57585b"}}>• YouTube</div>
+                
+                </div>
+
+
+                <div>
+                <img src={Line} style={{ width:"100%", maxWidth:"200px"}}/>
+                <br />
+                <span className={classes.bold} style={{color:"#f15953"}}>6 MONTH AGREEMENT</span>
+                <br /><br />
+
+
+                <div className={classes.bold} style={{color:"#f15953", paddingTop:"28px"}}>Monthly: <br /><span style={{textDecoration:"line-through"}}>$899/m</span> <span style={{color:"#f97306"}} className={classes.bold}>$749/m</span></div>
+<div className={classes.bold}  style={{color:"#f15953",}}>One Time Set-Up: <span style={{textDecoration:"line-through"}}>$800</span> <span style={{color:"#f97306"}} className={classes.bold}>$500</span></div>
+
+                
+<a target="_blank" href="" className={classes.getStartedLinkignite}>
+                  SUBSCRIBE NOW
+                    </a>
+                  
+                </div>
+                
+          </div>
+          </div>
+
+
+          </div>
+      </Container>
+
+<Container>
+<div style={{ textAlign: "center", padding: "35px", fontSize:"12px", lineHeight:"18px" }}>
+By signing this form, I expressly consent that Elm Street Technology or its agents may contact me at the numbers provided regarding the Elevate products or services, via live, automated or prerecorded call, text, or email. I am not requiredto enter into this agreement as a condition of any purchase. I can revoke this consent through any reasonable means.  By signing this form, I also agree to payment for the products and services ordered hereon and that sales tax may be charged where applicable. I also agree to the terms of cancellation policy on the back of this form including that set-up fees are non-refundable.  I confirm I have read, understood and agree to the full Terms of Service of the Elevate productsI am purchasing as set forth at TryElevate.com.  Visit TryElevate.com for full Terms of Service, Refund Policy, Privacy Policy &amp; DMCA Notice. Questions? - 877.218.9771. Sales tax charged where applicable.  Please note that setup fees are non-refundable.  <a target="_blank" href="http://tryelevate.com/terms-of-service/" >Full Terms of Service</a><br /><br />
+</div>
+</Container>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+
+
+
+
+
+  
+
+
+
+  );
+};
+
+
+export default withStyles((theme) => ({
+  root: {
+    position: "relative",
+    paddingTop: "66px",
+    paddingBottom: "0px",
+    lineHeight: "1.75em",
+  },
+  top: {
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "top",
+  },
+  section:{
+    paddingTop: "60px",
+  },
+  heading: {
+    color: theme.colors.secondary,
+    fontSize: "32px",
+    lineHeight: "40px",
+    fontWeight: "700",
+    marginRight: "auto",
+    paddingBottom: "20px",
+  },
+  heading2: {
+    color: theme.colors.secondary,
+    fontSize: "22px",
+    fontWeight: "700",
+    marginRight: "auto",
+    paddingBottom: "20px",
+  },
+  centerHeading:{
+    alignItems: "center",
+    width: "100%",
+    textAlign: "center",
+  },
+  bold:{
+    fontWeight:"700",
+  },
+  column:{
+    width:"525px",
+    maxWidth: "100%",
+    paddingRight:"18px",
+    paddingLeft:"18px",
+    verticalAlign: "top",
+  },
+  
+  column2:{
+    width:"710px",
+    maxWidth: "100%",
+    paddingRight:"10px",
+    paddingLeft:"10px",
+    verticalAlign: "top",
+  },
+
+  column3:{
+    width:"300px",
+    maxWidth: "100%",
+    paddingRight:"10px",
+    paddingLeft:"10px",
+    verticalAlign: "top",
+  },
+
+  column3a:{
+    width:"350px",
+    maxWidth: "100%",
+    paddingRight:"10px",
+    paddingLeft:"10px",
+    verticalAlign: "top",
+  },
+
+  getStartedLink: {
+    minWidth: "300px",
+    maxWidth: "300px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: theme.colors.secondary,
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "14px 16px",
+    borderRadius: "4px",
+    margin: "16px auto",
+    marginBottom: "20px",
+  },
+  getStartedLinksp: {
+    minWidth: "200px",
+    maxWidth: "200px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#56595B",
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "4px",
+    margin: "14px auto",
+    marginBottom: "20px",
+  },
+  getStartedLinkdms: {
+    minWidth: "200px",
+    maxWidth: "200px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#5BC1B8",
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "4px",
+    margin: "14px auto",
+    marginBottom: "20px",
+  },
+  getStartedLinkignite: {
+    minWidth: "200px",
+    maxWidth: "200px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#f99406",
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "4px",
+    margin: "14px auto",
+    marginBottom: "20px",
+  },
+  getStartedLinkelite: {
+    minWidth: "200px",
+    maxWidth: "200px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#EE5A55",
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "4px",
+    margin: "14px auto",
+    marginBottom: "20px",
+  },
+  getStartedLinkpro: {
+    minWidth: "200px",
+    maxWidth: "200px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#524493",
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "12px 14px",
+    borderRadius: "4px",
+    margin: "14px auto",
+    marginBottom: "20px",
+  },
+
+  getStartedLink2: {
+    minWidth: "300px",
+    maxWidth: "300px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#ffffff",
+    color: "#5dc4ba",
+    textDecoration: "none",
+    padding: "14px 16px",
+    borderRadius: "4px",
+    margin: "16px auto",
+    marginBottom: "20px",
+  },
+  darkTealLink: {
+    width: "250px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    letterSpacing: ".25px",
+    backgroundColor: theme.colors.secondaryDark,
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "14px 16px",
+    borderRadius: "4px",
+    margin: "16px auto",
+  },
+  grayLink: {
+    width: "250px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "14px",
+    fontWeight: "700",
+    letterSpacing: ".25px",
+    backgroundColor: theme.colors.gray400,
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "14px 16px",
+    borderRadius: "4px",
+    margin: "16px auto",
+  },
+  grayText:{
+    color: theme.colors.gray800,
+  },
+  infoboxGray: {
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: "#1f194c",
+    color: theme.colors.gray800,
+    textDecoration: "none",
+    padding: "40px 25px 25px 25px",
+    borderRadius: "4px",
+    margin: "30px auto",
+    textAlign: "center",
+  },
+  infoboxGray2: {
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: "#fafafa",
+    color: theme.colors.gray800,
+    textDecoration: "none",
+    padding: "30px 15px 15px 15px",
+    borderRadius: "4px",
+    margin: "30px auto",
+    textAlign: "center",
+  },
+  infoboxTeal: {
+    alignContent: "center",
+    justifyContent: "center",
+    backgroundColor: theme.colors.secondary,
+    color: theme.colors.gray800,
+    textDecoration: "none",
+    padding: "40px 25px 25px 25px",
+    borderRadius: "4px",
+    margin: "30px auto",
+    textAlign: "center",
+  },
+  videoBorder: {
+    borderStyle:"solid",
+    borderWidth: "1px",
+    borderColor: theme.colors.gray400,
+  },
+  iconPosition:{
+    position: "relative",
+    top:"12px",
+    right:"4px"
+  },
+  productCol: {
+    minHeight: "180px",
+
+    [theme.breakpoints[900]]: {
+      minHeight: "380px",
+    },
+  },
+}))(subscribe_today);
