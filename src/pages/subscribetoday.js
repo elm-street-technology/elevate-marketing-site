3831b5cd0924ef2d9c63c4b7f3163aac83e452c7
Kevin Blank <kblank@gmail.com>
2020-04-01 06:44:01 -0500

    April special (#128)
    * April Special Lander
    * Norm Webinar Lander
    
    Co-authored-by: Matt Litchliter <mattlitchliter@Matts-MacBook-Pro.local>
    Co-authored-by: Kevin Blank <kevinblank@Kevins-MacBook-Pro.local>

--- src/pages/subscribetoday.js
+++ src/pages/subscribetoday.js
@@ -41,7 +41,23 @@ const subscribe_today = ({ classes }) => {
     
 
 
+      <div style={{backgroundImage:"url("+Aprilbg+")", backgroundSize:"cover", backgroundPosition:"centerTop"}}>
+<Container>
+      <div className={classes.top} style={{paddingBottom:"60px", paddingRight:"35px", paddingLeft:"35px", paddingTop:"55px"}}>
+          <div className={classes.centerHeading}>
+                            <div className={classes.bold} style={{color:"#fbda3d", fontSize:"32px", lineHeight:"45px"}}>APRIL SPECIAL OFFER</div>
+                            <div className={classes.bold} style={{color:"#ffffff", fontSize:"18px", lineHeight:"33px"}}>Serving brokers &amp; agents in challenging times</div>
+                            <div style={{color:"#ffffff", fontSize:"12px", paddingBottom:"15px"}}>*valid for all NEW subscribers ONLY</div>
 
+                            <div className={classes.heading} style={{color:"#fbda3d"}}>50% OFF YOUR 1ST 3-MONTHS</div>
+                            <div className={classes.bold} style={{color:"#ffffff", fontSize:"18px", lineHeight:"33px"}}><span style={{color:"#fbda3d"}}>ANY</span> Elevate Offering (Marketing and/or Lead Gen.)
+                            </div>
+                            <div className={classes.bold} style={{color:"#fbda3d", fontSize:"18px",}}>USE CODE:  50april2020</div>
+                            <div className={classes.bold} style={{color:"#ffffff", fontSize:"18px", lineHeight:"33px"}}>during check-out process to redeem</div>
+          </div>
+      </div>
+</Container>
+</div>
 
 
 
---
+++ src/pages/thenewnorm.js
@@ -0,0 +1,182 @@
+import React from "react";
+import Helmet from "react-helmet";
+import withStyles from "elevate-ui/withStyles";
+import config from "../utils/siteConfig";
+import Container from "../components/Container";
+import SignUpFormNorm from "../components/SignUpFormNorm";
+import Covid from "../images/covid_header.jpg";
+import Norm from "../images/norm.png";
+import C1 from "../images/covid_c1.png";
+import C2 from "../images/covid_c2.png";
+import C3 from "../images/covid_c3.png";
+import C4 from "../images/covid_c4.png";
+import SEO from "../components/SEO";
+
+const Thenewnorm = ({ children, classes, data, tags }) => {
+  const postNode = {
+    title: `The New Norm - ${config.siteTitle}`,
+  };
+
+  return (
+    <div className={classes.root}>
+      <Helmet>
+        <title>{`Thenewnorm - ${config.siteTitle}`}</title>
+      </Helmet>
+      <SEO postNode={postNode} pagePath="Thenewnorm" />
+
+
+<Container>
+    <div style={{paddingBottom:"50px", textAlign:"center"}}>
+    <img src={Norm} width="325" style={{paddingBottom:"45px"}}/>
+      <br />
+
+      <h1 className={classes.heading}>Stream The Webinar</h1>
+            <h2 className={classes.subHeading}>
+             Missed the webinar or looking for a replay? We’ve got you covered.</h2>
+
+              <div style={{textAlign: "center", maxWidth:"700px",marginLeft:"auto",marginRight:"auto", paddingBottom:"50px"}}>
+                <div style={{ textAlign: "center"}} className={classes.videoEmbed}>
+                        <iframe className={classes.videoContainer} src="https://www.youtube.com/embed/g92eC1CVAYs"
+                         frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+              </div>
+
+
+             
+
+
+            
+
+
+
+
+
+
+                  
+                  
+                
+             
+
+
+
+
+
+
+
+    </div>
+</Container>
+
+
+
+ <div style={{backgroundColor:"#f7f7f7", paddingBottom:"50px"}}>
+        <h1 className={classes.heading} style={{paddingTop:"50px"}}>
+        Connect with a Success Coach
+        </h1>
+        <SignUpFormNorm />
+        {/* <div className={classes.closingContainer}>
+          For Media Inquiries, {""} Please {""}
+          <a
+            className={classes.emailLink}
+            href="mailto:ekaplan@elmstreettechnology.com"
+          >
+            Email Us
+          </a>
+        </div> */}
+ </div>
+    
+</div>
+  );
+};
+
+export default withStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    padding: "0 0px",
+    margin: "40px auto",
+  },
+  heading: {
+    fontSize: "28px",
+    fontWeight: "700",
+    letterSpacing: ".14px",
+    color: "#55c3ba",
+    textAlign: "center",
+    marginBottom: "12px",
+  },
+  top: {
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "top",
+  },
+
+  bold:{
+    fontWeight:"700",
+  },
+
+  column3:{
+    width:"350px",
+    maxWidth: "100%",
+    paddingRight:"10px",
+    paddingLeft:"10px",
+    verticalAlign: "top",
+  },
+
+  column2:{
+    width:"470px",
+    maxWidth: "100%",
+    paddingRight:"10px",
+    paddingLeft:"10px",
+    verticalAlign: "top",
+  },
+
+  subHeading: {
+    fontSize: "18px",
+    lineHeight: "28px",
+    fontWeight: "600",
+    letterSpacing: ".08px",
+    color: "#888f96",
+    textAlign: "center",
+    margin: "0 auto 36px",
+  },
+
+  covidContainer: {
+    width: "70%",
+    margin: "16px auto 0 auto",
+  },
+  covidHeader: {
+    width: "100%",
+  },
+
+  videoEmbed:{
+    position: "relative",
+    paddingBottom: "56.25%",
+    paddingTop: "30px",
+    height: "0",
+    overflow: "hidden",
+    
+  },
+  videoContainer:{
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    
+  }
+  // closingContainer: {
+  //   color: "#A9A8B6",
+  //   fontWeight: "600",
+  //   fontSize: "18px",
+  //   textAlign: "center",
+  //   margin: "60px auto 0",
+  // },
+  // emailLink: {
+  //   color: "#2E2E35 !important",
+  //   textDecoration: "underline",
+
+  //   "&:hover": {
+  //     textDecoration: "none !important",
+  //   },
+  // },
+}))(Thenewnorm);
