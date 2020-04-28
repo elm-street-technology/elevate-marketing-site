f63c6d657eb530ec598f1f5d54c1b62dc37ffb2d
Matt Litchliter <mattlitchliter@Matts-MacBook-Pro.local>
2020-04-28 12:34:09 -0500

    two new landers

---
+++ src/components/SignUpFormBalance.js
@@ -0,0 +1,325 @@
+import React, { Component } from "react";
+import classNames from "classnames";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+import Alert from "elevate-ui/Alert";
+import CheckboxGroup from "elevate-ui/CheckboxGroup";
+import Input from "elevate-ui/Input";
+import RadioGroup from "elevate-ui/RadioGroup";
+import Typography from "elevate-ui/Typography";
+import withStyles from "elevate-ui/withStyles";
+import Datetime from "elevate-ui/Datetime";
+import moment from "moment";
+
+class SignUpFormBalance extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      formState: null,
+    };
+  }
+
+  render() {
+    const { formState } = this.state;
+    const { classes, className } = this.props;
+
+    var valid = function (current) {
+      return current.day() !== 0 && current.day() !== 6;
+    };
+
+    var renderDay = function (props, currentDate, selectedDate) {
+
+      if (currentDate.month() == moment().month()) {
+        if (currentDate.date() < moment().date()) {
+          if (props.className == "rdtDay") {
+            props.className = "rdtDay rdtDisabled";
+          }
+        }
+      }
+      if (currentDate.month() < moment().month()) {
+        props.className = "rdtDay rdtDisabled";
+      }
+
+      return <td {...props}>{currentDate.date()}</td>;
+    };
+
+    if (formState === "success") {
+      return (
+        <div
+          style={{
+            maxWidth: "400px",
+            textAlign: "center",
+            margin: "24px auto",
+          }}
+        >
+          <Alert color="success">
+            <Typography type="heading3" gutterBottom>
+              AWESOME.
+            </Typography>
+            <Typography type="heading6" gutterBottom>
+              An Elevate sales representative will be in touch with you shortly.
+            </Typography>
+            <Typography type="heading4" gutterTop>
+              If you'd like to speak to a sales representative NOW, please call{" "}
+              <a href="tel:18447920260" className={classes.link}>
+                844.792.0260
+              </a>.
+            </Typography>
+          </Alert>
+        </div>
+      );
+    } else if (formState === "error") {
+      return (
+        <div style={{ maxWidth: "400px", margin: "24px auto" }}>
+          <Alert color="error">An error occurred.</Alert>
+        </div>
+      );
+    }
+
+    return (
+      <div className={classNames(classes.root, className)}>
+        <Formik
+          initialValues={{
+            firstname: "",
+            lastname: "",
+            company: "",
+            email: "",
+            phone: "",
+            role: "",
+            roleOther: "",
+            interests: [],
+            form: "covidshift",
+            list: 97842,
+          }}
+          validationSchema={() =>
+            Yup.object().shape({
+              firstname: Yup.string().required("First name is required"),
+              lastname: Yup.string().required("Last name is required"),
+              company: Yup.string(),
+              email: Yup.string()
+                .email("Invalid email address")
+                .required("Email is required"),
+              phone: Yup.string().required("Phone is required"),
+              role: Yup.string(),
+              roleOther: Yup.string(),
+              interests: Yup.array(),
+            })
+          }
+          onSubmit={(values, { setSubmitting }) => {
+            var meeting_request = '';
+            Object.keys(values).forEach(function (key, index) {
+              // key: the name of the object key
+              // index: the ordinal position of the key within the object 
+              if (key == 'meetingdate') {
+                if (typeof values.meetingdate.format === "function") {
+                  meeting_request = values.meetingdate.format("YYYY-MM-DD") + "T" + values.meetingtime.replace(" (EDT)", "") + "-04:00"
+                }
+              }
+            });
+            const body = {
+              ...values,
+              roleOther: values.role === "Other" ? values.roleOther : "", // Just in case the user had typed in roleOther then changed their role to something else
+              interestStr: values.interests.join(",") + ", Interested Markets: " + values.market1 + ", " + values.market2 + "," + values.market3,
+              utm_campaign: (window.utm_tags) ? window.utm_tags.campaign : "",
+              utm_source: (window.utm_tags) ? window.utm_tags.source : "",
+              utm_medium: (window.utm_tags) ? window.utm_tags.medium : "",
+              utm_term: (window.utm_tags) ? window.utm_tags.term : "",
+              demo_request_date: meeting_request
+            };
+            return fetch(
+              "https://easyemerge.com/plugins/elevate_form.php",
+              {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8",
+                },
+                body: JSON.stringify(body),
+              }
+            )
+              .then((response) => response.json())
+              .then((res) => {
+                if (res.message === "ok") {
+                  this.setState({ formState: "success" });
+
+                  if (window.fbq) {
+                    window.fbq("track", "Lead");
+                  }
+                  if (window.gtag) {
+                    window.gtag("event", "conversion", {
+                      send_to: "AW-980447970/2_aHCOnsjo8BEOLlwdMD",
+                    });
+                  }
+                  if (window.ga) {
+                    window.ga('send','event','form','form_completed','get-started');
+                  }
+                } else {
+                  this.setState({ formState: "error" });
+                }
+              })
+              .catch((err) => {
+                this.setState({ formState: "error" });
+              });
+          }}
+          render={({ values, isSubmitting, handleBlur, handleChange }) => (
+            <Form noValidate>
+              <Typography
+                type="heading6"
+                style={{ textAlign: "center", marginBottom: "32px", color: "#888f96" }}
+              >
+                We’re here to help you succeed during (and after) this market shift.
+                
+              </Typography>
+              <div className={classes.topRow}>
+                <Field
+                  id="firstname"
+                  name="firstname"
+                  label="First Name"
+                  component={Input}
+                  className={classes.field}
+                />
+                <Field
+                  id="lastname"
+                  name="lastname"
+                  label="Last Name"
+                  component={Input}
+                  className={classes.field}
+                />
+              </div>
+              <Field
+                id="email"
+                name="email"
+                label="Email"
+                component={Input}
+                className={classes.field}
+              />
+              <Field
+                id="phone"
+                name="phone"
+                label="Phone"
+                component={Input}
+                className={classes.field}
+                type="tel"
+              />
+              <Field
+                id="company"
+                name="company"
+                label="Affiliation (optional)"
+                component={Input}
+                className={classes.field}
+              />
+              <Field
+                id="mls_number"
+                name="mls_number"
+                label="MLS # (optional)"
+                component={Input}
+                className={classes.field}
+              />
+              <Field
+                id="role"
+                name="role"
+                label="I am a(n)"
+                component={RadioGroup}
+                display="inline"
+                items={[
+                  {
+                    label: "Existing client – looking to connect to review my account & make sure I’m doing everything I can!",
+                    value: "Existing Client",
+                  },
+                  {
+                    label: "An agent – looking to schedule my 15-minute online marketing evaluation.",
+                    value: "Agent",
+                  },
+                  {
+                    label: "A broker – interested in scheduling my gap analysis.",
+                    value: "Broker",
+                  },
+                  {
+                    label: "Other (please specify)*",
+                    value: "Other",
+                  },
+                ]}
+                className={classes.field}
+              />
+              {values.role === "Other" && (
+                <Field
+                  id="roleOther"
+                  name="roleOther"
+                  label="Other"
+                  component={Input}
+                  className={classes.field}
+                  autoFocus
+                />
+              )}
+              
+              <button
+                type="submit"
+                className={classes.signUpBtn}
+                disabled={isSubmitting}
+              >
+                Please contact me asap
+              </button>
+              <div style={{ fontSize: "11px",textAlign: "center" }}>
+              By submitting this form, you are requesting to be contacted by a member of the Elevate Sales Team at the details provided via text, email or call (may involve automated or pre-recorded means).  You may revoke this consent through any reasonable means.
+              </div>
+            </Form>
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+export default withStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    maxWidth: "600px",
+    backgroundColor: "#f7f7f7 !important",
+    margin: "0 auto",
+  },
+  link: {
+    color: "inherit",
+  },
+  field: {
+    borderRadius: "6px",
+    border: "2px solid #ECECEC",
+    height: "50px",
+    fontWeight: "600",
+  },
+  topRow: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+
+    "& > * + *": {
+      marginLeft: "12px",
+    },
+  },
+  signUpBtn: {
+    width: "100%",
+    fontSize: "20px",
+    lineHeight: "26px",
+    fontWeight: "600",
+    color: "#FFF",
+    backgroundColor: theme.colors.secondary,
+    borderRadius: "6px",
+    padding: "12px",
+    margin: "14px 0",
+  },
+  selectfield: {
+    borderRadius: "6px",
+    border: "2px solid #ECECEC",
+    height: "40px"
+  },
+  selectlabel: {
+    width: "100%",
+    color: "#888f96",
+    display: "flex",
+    fontSize: "14px",
+    alignItems: "center",
+    lineHeight: "18px",
+    fontWeight: "700",
+    marginBottom: "4px",
+  },
+}))(SignUpFormBalance);
