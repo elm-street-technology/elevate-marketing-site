07eda603d86e3a2ff205fd66f7301d44522f04a8
Matt Litchliter <mattlitchliter@Matts-MacBook-Pro.local>
2020-05-14 15:09:06 -0500

    embed update

--- src/pages/rollercoaster.js
+++ src/pages/rollercoaster.js
@@ -43,7 +43,7 @@ const Rollercoaster = ({ children, classes, data, tags }) => {
 
               <div style={{textAlign: "center", maxWidth:"700px",marginLeft:"auto",marginRight:"auto", paddingBottom:"50px"}}>
                 <div style={{ textAlign: "center"}} className={classes.videoEmbed}>
-                        <iframe className={classes.videoContainer} src="https://www.youtube.com/embed/F4leasNVHPY"
+                        <iframe className={classes.videoContainer} src="https://www.youtube.com/embed/MQ4bcqLANjc"
                          frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
               </div>
