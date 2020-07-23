11166394ea1ad7f6d8f4bbc7a36d9e74685ca042
Kevin Blank <kevinblank@Kevins-MacBook-Pro.local>
2020-07-23 13:45:52 -0500

    Moved adRoll to onRouteUpdate

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -40,6 +40,44 @@ exports.onRouteUpdate = ({ location, prevLocation }) => {
     // End leadfollow.io tracking reload
 
     // AdRoll Integration here
+    const adRollHtml = `
+    adroll_adv_id = "QCK3V4BRFZB7VCNZ7UJ4QV";
+    adroll_pix_id = "PUTRQDC72JGJ7GFETYTPUW";
+    adroll_version = "2.0";
+
+    (function(w, d, e, o, a) {
+        w.__adroll_loaded = true;
+        w.adroll = w.adroll || [];
+        w.adroll.f = [ 'setProperties', 'identify', 'track' ];
+        var roundtripUrl = "https://s.adroll.com/j/" + adroll_adv_id
+                + "/roundtrip.js";
+        for (a = 0; a < w.adroll.f.length; a++) {
+            w.adroll[w.adroll.f[a]] = w.adroll[w.adroll.f[a]] || (function(n) {
+                return function() {
+                    w.adroll.push([ n, arguments ])
+                }
+            })(w.adroll.f[a])
+        }
+
+        e = d.createElement('script');
+        o = d.getElementsByTagName('script')[0];
+        e.async = 1;
+        e.src = roundtripUrl;
+        o.parentNode.insertBefore(e, o);
+    })(window, document);
+    adroll.track("pageView");
+    `;
+    var ExistingAdRollElement = document.getElementById('AdRoll');
+
+    if(ExistingAdRollElement != null){
+        ExistingAdRollElement.outerHTML = '';
+    }
+    var addRollElement = document.createElement('SCRIPT');
+    addRollElement.id = 'AdRoll';
+    addRollElement.innerHTML = adRollHtml;
+    if (document.body != null) { 
+        document.body.appendChild(addRollElement); 
+    }
 
 };
 
