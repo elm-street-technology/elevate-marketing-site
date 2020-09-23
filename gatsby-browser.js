ae2674f636d0bcdf2ae98356aa8a4afd08642d48
Kevin Blank <kevinblank@Kevins-MacBook-Pro.local>
2020-09-23 12:45:25 -0500

    Turning off leadfollow

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -25,6 +25,7 @@ exports.onRouteUpdate = ({ location, prevLocation }) => {
     // The follow code is to hot reload the leadfollow tracking pixel on route update
     // The leadfollow.io API doesn't seem to have a trigger or function to handle this in the same 
     // way we handle FB or GA so pure DOM manipulation will have to suffice
+    /*
     var LFElement = document.getElementById('LFTracking');
 
     if(LFElement != null){
@@ -37,6 +38,7 @@ exports.onRouteUpdate = ({ location, prevLocation }) => {
     if (document.body != null) { 
         document.body.appendChild(new_element); 
     }
+    */
     // End leadfollow.io tracking reload
 
     // AdRoll Integration here
