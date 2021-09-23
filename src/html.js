51ef81992052853cd8463c0a9a0e688b331c2f6b
Kevin Blank <kblank@tryelevate.com>
2021-09-23 16:41:47 -0500

    Fixing gtag

--- src/html.js
+++ src/html.js
@@ -33,6 +33,17 @@ module.exports = class HTML extends React.Component {
           {this.props.headComponents}
           {css}
           <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-KNZFKDR');
+              `,
+            }}
+          />
+          <script
             defer
             src="https://easyemerge.com/landing_src/emerge.js"
           />
@@ -47,19 +58,8 @@ module.exports = class HTML extends React.Component {
         <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KNZFKDR" height="0" width="0" style={{display:"none",visibility:"hidden"}}></iframe></noscript>
           <script aysnc src="https://www.eventbrite.com/static/widgets/eb_widgets.js" />
           
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `
-                <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KNZFKDR');</script>
-<!-- End Google Tag Manager --> 
-              `,
-            }}
-          />
+
+          
           <script
             dangerouslySetInnerHTML={{
               __html: `
