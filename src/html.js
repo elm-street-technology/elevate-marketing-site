e97f0cdaa80b543a08469fe103c656bd7f7cdf3d
Kevin Blank <kblank@tryelevate.com>
2021-09-23 15:54:46 -0500

    New GTag tracking

--- src/html.js
+++ src/html.js
@@ -45,20 +45,18 @@ module.exports = class HTML extends React.Component {
         </head>
         <body {...this.props.bodyAttributes}>
           
-          
-          <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=AW-980447970"
-          />
           <script aysnc src="https://www.eventbrite.com/static/widgets/eb_widgets.js" />
+          
           <script
             dangerouslySetInnerHTML={{
               __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', 'AW-980447970');
+                <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KNZFKDR');</script>
+<!-- End Google Tag Manager --> 
               `,
             }}
           />
