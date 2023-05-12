7c6ed03384d2e9b3697b4f0f821c39757f65ee2c
Matt Litchliter <matt@easyemerge.com>
2023-05-12 10:36:16 -0500

    elevate crm lander added

---
+++ src/pages/success.js
@@ -0,0 +1,443 @@
+import React from "react";
+import Helmet from "react-helmet";
+import withStyles from "elevate-ui/withStyles";
+import config from "../utils/siteConfig";
+import Container from "../components/Container";
+import BgSectionExternal3 from "../components/BgSectionExternal3";
+import FormProduct from "../components/FormProduct";
+import Glancebg from "../images/glancebg.jpg";
+import Fadebg from "../images/fadebg.jpg";
+import Dmslogo from "../images/dmslogo.png";
+import Dmsstar from "../images/dmsstar.png";
+import Quote from "../images/quote.png";
+import Sp1 from "../images/sp1.png";
+import Cma from "../images/prodcma.png";
+import Dms2 from "../images/dms2.png";
+import Dms3 from "../images/dms3.png";
+import Dms5 from "../images/dms5.gif";
+import Dms7 from "../images/dms7.png";
+import Elevatew from "../images/elevate_tagw.svg";
+import Sp3 from "../images/sp3.png";
+import Sp5 from "../images/sp5.png";
+import Ign2 from "../images/ign2.gif";
+import Glance4 from "../images/glance4.png";
+import Glance5 from "../images/glance5.png";
+import SEO from "../components/SEO";
+
+const Success = ({ children, classes, data, tags }) => {
+  const postNode = {
+      title: `CRM Success with Elevate - ${config.siteTitle}`,
+  };
+
+  return (
+<div className={classes.root}>
+      <Helmet>
+              <title>{`CRM Success with Elevate - ${config.siteTitle}`}</title>
+      </Helmet>
+      <SEO postNode={postNode} pagePath="Success" customTitle />
+
+<div style={{backgroundImage:"url("+Glancebg+")", backgroundSize:"cover", backgroundPosition:"centerTop"}}>
+<Container>
+      <div className={classes.top} style={{paddingBottom:"60px", paddingRight:"35px", paddingLeft:"35px", paddingTop:"75px"}}>
+          <div className={classes.centerHeading}>
+          <img src={Elevatew} style={{width:"100%", maxWidth:"250px", paddingBottom:"20px"}}/>
+
+                            <div className={classes.heading} style={{color:"#ffffff", paddingBottom:"10px"}}>The Power of Marketing</div>
+                            <div className={classes.subtext2} style={{color:"#f7f7f7", fontSize:"18px", lineHeight:"24px"}}>Be in front of more people, more often.
+
+<div style={{ textAlign: "center", paddingTop: "24px" }}>
+                 <a href="#demo" className={classes.buttonLink}>Request a Demo</a>
+            </div>
+                            </div>
+          </div>
+      </div>
+
+         
+
+
+
+</Container>
+</div>
+
+
+
+
+
+
+
+
+<div style={{paddingTop:"68px",}}>
+
+            <Container>
+
+                        <div style={{textAlign:"center", paddingRight:"35px", paddingLeft:"35px", paddingBottom:"35px"}}>
+                            <div className={classes.Heading2}>Key Highlights</div>
+                            <div className={classes.subtext}>Build your business, increase your sphere &amp; look like<br />
+                            a marketing rockstar with an all-in-one solution designed to capture, engage &amp; convert.</div>     
+                            
+                        </div>
+            </Container>
+
+</div>
+<div style={{backgroundImage:"url("+Fadebg+")", backgroundSize:"cover", backgroundPosition:"centerBottom"}}>
+<Container>
+<div className={classes.top} style={{textAlign:"center"}}>
+
+
+<div className={classes.column3} style={{ textAlign: "center", paddingTop: "20px" }}>
+<div className={classes.subtext2}>Advanced, intuitive CRM with<br />
+powerful performance metrics</div>
+<div style={{ textAlign: "center",}}>
+    <img src={Ign2} width="100%"  style={{paddingTop:"25px", maxWidth:"320px"}} />
+</div>
+</div>
+
+<div className={classes.column3} style={{ textAlign: "center", paddingTop: "20px" }}>
+<div className={classes.subtext2}>Morning reports with<br />
+the latest activity data.</div>
+<div style={{ textAlign: "center",}}>
+    <img src={Dms2} width="100%"  style={{paddingTop:"25px", maxWidth:"320px"}} />
+</div>
+</div>
+
+<div className={classes.column3} style={{ textAlign: "center", paddingTop: "20px" }}>
+<div className={classes.subtext2}>Mobile-friendly<br />
+IDX website</div>
+<div style={{ textAlign: "center",}}>
+    <img src={Dms3} width="100%"  style={{paddingTop:"25px", maxWidth:"320px"}} />
+</div>
+</div>
+
+
+</div>
+</Container>
+</div>
+
+<div style={{paddingTop:"40px", backgroundImage:"url("+Fadebg+")", backgroundSize:"cover", backgroundPosition:"centerBottom"}}>
+<Container>
+<div className={classes.top} style={{textAlign:"center"}}>
+
+
+<div className={classes.column3} style={{ textAlign: "center", paddingTop: "20px" }}>
+<div className={classes.subtext2}>Robust blog with<br />
+automated posts</div>
+<div style={{ textAlign: "center",}}>
+    <img src={Sp5} width="100%"  style={{paddingTop:"25px", maxWidth:"320px"}} />
+</div>
+</div>
+
+<div className={classes.column3} style={{ textAlign: "center", paddingTop: "20px" }}>
+<div className={classes.subtext2}>Automated social media<br />
+posts weekly</div>
+<div style={{ textAlign: "center",}}>
+    <img src={Dms5} width="100%"  style={{paddingTop:"25px", maxWidth:"320px"}} />
+</div>
+</div>
+
+<div className={classes.column3} style={{ textAlign: "center", paddingTop: "20px" }}>
+<div className={classes.subtext2}>Intuitive<br />
+CMA Wizard</div>
+<div style={{ textAlign: "center",}}>
+    <img src={Cma} width="100%"  style={{paddingTop:"25px", maxWidth:"320px"}} />
+</div>
+</div>
+
+
+</div>
+</Container>
+</div>
+
+<div style={{paddingTop:"40px", backgroundImage:"url("+Fadebg+")", backgroundSize:"cover", backgroundPosition:"centerBottom"}}>
+<Container>
+<div className={classes.top} style={{textAlign:"center"}}>
+
+
+<div className={classes.column3} style={{ textAlign: "center", paddingTop: "20px" }}>
+<div className={classes.subtext2}>Email marketing templates,<br />
+campaigns &amp; image library</div>
+<div style={{ textAlign: "center",}}>
+    <img src={Dms7} width="100%"  style={{paddingTop:"25px", maxWidth:"320px"}} />
+</div>
+</div>
+
+<div className={classes.column3} style={{ textAlign: "center", paddingTop: "20px" }}>
+<div className={classes.subtext2}>Customized social branding on Facebook,<br />
+Twitter, LinkedIn &amp; YouTube</div>
+<div style={{ textAlign: "center",}}>
+    <img src={Sp1} width="100%"  style={{paddingTop:"25px", maxWidth:"320px"}} />
+</div>
+</div>
+
+<div className={classes.column3} style={{ textAlign: "center", paddingTop: "20px" }}>
+<div className={classes.subtext2}>Automated creation of<br />
+YouTube video tours</div>
+<div width="100%" style={{textAlign: "center", maxWidth:"320px",marginLeft:"auto",marginRight:"auto", paddingTop:"25px" }}>
+                <div style={{ textAlign: "center"}} className={classes.videoEmbed}>
+                        <iframe className={classes.videoContainer} src="https://www.youtube.com/embed/CwXlC7Xuojs"
+                         frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+              </div>
+</div>
+
+
+</div>
+</Container>
+</div>
+
+
+
+<a id="demo"></a>
+<div style={{backgroundColor:"#ffffff", paddingBottom:"60px", textAlign:"center", paddingRight:"30px", paddingLeft:"30px"}}>
+        <h1 className={classes.Heading2} style={{paddingTop:"80px"}}>
+        Take a Tour of Elevate Today
+        </h1>
+        <h2 className={classes.subtext}>Schedule 15-minutes with an Elevate Success Coach to explore Elevate's marketing power.</h2>
+        <FormProduct
+          subject="DMS Product Lead"
+          listid="107387"
+          webhook="b1452e08-f398-11ea-9d14-bc764e046a9d"
+          leadsource="tryelevate.com:elevatedms" />
+        {/* <div className={classes.closingContainer}>
+          For Media Inquiries, {""} Please {""}
+          <a
+            className={classes.emailLink}
+            href="mailto:ekaplan@elmstreettechnology.com"
+          >
+            Email Us
+          </a>
+        </div> */}
+ </div>
+
+
+<div style={{paddingTop:"70px", paddingBottom:"60px", backgroundColor:"#f7f7f7"}}>
+        
+                        <div style={{textAlign:"center", paddingRight:"35px", paddingLeft:"35px", paddingBottom:"25px"}}>
+                            <div className={classes.Heading2}>The Power of Success</div>
+                            <div className={classes.subtext}>Nothing feels better than happy clients celebrating<br />
+their productivity, success &amp; growth using Elevate.</div>     
+                            
+                        </div>
+    <Container>
+            <div className={classes.flexContainer}>
+                <div className={classes.flexItem2} style={{padding:"30px", textAlign:"center"}}>
+                        <img src={Quote} width="80" style={{paddingBottom:"15px"}}/>
+                      <div className={classes.subtext}>
+                      “My Elevate website is so easy for my customers to use...and I LOVE the daily marketing report emails.”
+                      <br /><br />
+                      <div className={classes.Heading3}>Mark H.</div>
+                      REALTOR&reg; - Tucson, AZ
+                      </div>
+                </div>
+
+                <div className={classes.flexItem2} style={{padding:"30px", textAlign:"center"}}>
+                <img src={Quote} width="80" style={{paddingBottom:"15px"}}/>
+                      <div className={classes.subtext}>  
+                      “For the first time ever, I actually like using a CRM!  Elevate automates so much of my day-to-day.  It’s simple!”
+                      <br /><br />
+                      <div className={classes.Heading3}>Kendra D.</div>
+                      REALTOR&reg; - Denver, CO.
+                        </div>
+                </div>
+
+                <div className={classes.flexItem2} style={{padding:"30px", textAlign:"center"}}>
+                <img src={Quote} width="80" style={{paddingBottom:"15px"}}/>
+                      <div className={classes.subtext}>  
+                      “All of my social media is done for me, and I love the email drip campaigns and newsletters for my clients.”
+                      <br /><br />
+                      <div className={classes.Heading3}>Brenda T.</div>
+                      REALTOR&reg; -  Atlanta, GA
+                        </div>
+                </div>
+
+
+      </div>
+
+        
+    </Container>
+</div> 
+
+
+
+
+    </div>
+  );
+};
+
+export default withStyles((theme) => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100%",
+    marginTop: "0px",
+    paddingTop:"0px",
+  },
+
+  top: {
+    display: "flex",
+    flexWrap: "wrap",
+    alignItems: "top",
+  },
+
+  heading: {
+    fontSize: "44px",
+    fontWeight: "700",
+    lineHeight: "52px",
+    letterSpacing: ".14px",
+    color: "#52B1AD",
+    marginBottom: "12px",
+  },
+
+  subtext: {
+    fontSize: "16px",
+    fontWeight: "300",
+    lineHeight: "24px",
+    color: "#5A5B5C",
+  },
+
+  subtext2: {
+    fontSize: "16px",
+    fontWeight: "700",
+    lineHeight: "24px",
+    color: "#5A5B5C",
+  },
+
+  column2:{
+    width:"530px",
+    maxWidth: "100%",
+    paddingRight:"20px",
+    paddingLeft:"20px",
+    verticalAlign: "top",
+  },
+
+  column3:{
+    width:"350px",
+    maxWidth: "100%",
+    paddingRight:"10px",
+    paddingLeft:"10px",
+    verticalAlign: "top",
+  },
+
+  bold:{
+    fontWeight:"700",
+  },
+
+  centerHeading:{
+    alignItems: "center",
+    width: "100%",
+    textAlign: "center",
+  },
+
+  Heading2: {
+    fontSize: "30px",
+    paddingBottom: "20px",
+    fontWeight: "700",
+    lineHeight: "36px",
+    color: "#52B1AD",
+    letterSpacing: ".14px",
+  },
+
+  Heading3: {
+    fontSize: "20px",
+    paddingBottom: "5px",
+    fontWeight: "700",
+    lineHeight: "26px",
+    color: "#52B1AD",
+    letterSpacing: ".14px",
+  },
+
+  landingHeaderStyle:{
+    textAlign:"center",
+  },
+
+
+  buttonLink: {
+    minWidth: "250px",
+    maxWidth: "250px",
+    alignContent: "center",
+    justifyContent: "center",
+    fontSize: "16px",
+    fontWeight: "700",
+    display: "block",
+    letterSpacing: ".25px",
+    backgroundColor: "#52B1AD",
+    color: "#FFF",
+    textDecoration: "none",
+    padding: "16px",
+    borderRadius: "4px",
+    margin: "16px auto",
+    marginBottom: "10px",
+  },
+
+  flexContainer: {
+    padding: "0",
+    margin: '0',
+  
+    display: "-webkit-box",
+    display: "-moz-box",
+    display: "-ms-flexbox",
+    display: "-webkit-flex",
+    display: "flex",
+    flexWrap: "wrap",
+  
+  },
+  flexItem: {
+    maxWidth: "100%",
+    width: "100%",
+    paddingRight: "0px",
+    paddingBottom: "25px",
+  
+  
+    [theme.breakpoints[900]]: {
+        maxWidth: "50%",
+        width: "50%",
+        paddingRight: "20px",
+        paddingBottom: "0px",
+    },
+  },
+
+  flexItem2: {
+    maxWidth: "100%",
+    width: "100%",
+    paddingRight: "0px",
+    paddingBottom: "25px",
+  
+  
+    [theme.breakpoints[900]]: {
+        maxWidth: "33%",
+        width: "33%",
+        paddingRight: "20px",
+        paddingBottom: "0px",
+    },
+  },
+
+  flexItem3: {
+    maxWidth: "100%",
+    width: "100%",
+    paddingRight: "0px",
+    paddingBottom: "25px",
+  
+  
+    [theme.breakpoints[900]]: {
+        maxWidth: "40%",
+        width: "40%",
+        paddingRight: "20px",
+        paddingBottom: "0px",
+    },
+  },
+  videoEmbed:{
+    position: "relative",
+    paddingBottom: "56.25%",
+    paddingTop: "30px",
+    height: "0",
+    overflow: "hidden",
+    
+  },
+  videoContainer:{
+    position: "absolute",
+    top: "0",
+    left: "0",
+    width: "100%",
+    height: "100%",
+    
+  }
+}))(Success);
